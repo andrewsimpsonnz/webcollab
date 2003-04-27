50903905a6d4003aaabe2684f7d8d4787d358b16
andrewsimpson <andrew.simpson@paradise.net.nz>
2003-04-27 03:50:39 +0000

    Code rewritten to allow for SMTP mail transfer

--- webcollab/includes/email.php
+++ webcollab/includes/email.php
@@ -4,11 +4,8 @@
 
   WebCollab
   ---------------------------------------
-  Created as CoreAPM 2001/2002 by Dennis Fleurbaaij
-  with much help from the people noted in the README
-
-  Rewritten as WebCollab 2002/2003 (from CoreAPM Ver 1.11)
-  by Andrew Simpson <andrew.simpson@paradise.net.nz>
+  
+  This file wriiten 2003 by Andrew Simpson <andrew.simpson@paradise.net.nz>
 
   This program is free software; you can redistribute it and/or modify it under the
   terms of the GNU General Public License as published by the Free Software Foundation;
@@ -27,6 +24,10 @@
 
   Sends emails and preformatted emails
   
+  ** Parts of this code lifted from phpmailer (Chris Ryan) and from contributed notes in PHP Manual under fsockopen.
+  
+  Refer to RFC 821 and RFC 2045 for SMTP 
+  
 */
 
 //get our location
@@ -36,27 +37,131 @@ if( ! @require( "path.php" ) )
 include_once( BASE."includes/security.php" );
 include_once( BASE."includes/admin_config.php" );
 
-//
-// Sends a message
-//
 function email( $to, $subject, $message) {
 
-  global $EMAIL_FROM, $EMAIL_REPLY_TO;
+  global $EMAIL_FROM, $EMAIL_REPLY_TO, $USE_EMAIL, $MAIL_METHOD, $SMTP_HOST, $DOMAIN;
 
   if( ! valid_string($to) ) {
-    //no email address specified!
+    //no email address specified - end function
     return;
   }
-
-  if( ! mail( $to, $subject, $message, "From: ".$EMAIL_FROM."\nReply-To: ".$EMAIL_REPLY_TO."\nX-Mailer: PHP/" . phpversion() ) ) {
-
-    if( $to == $EMAIL_ERROR )
-      //prevent error loops due to faulty email system!
-      warning( "Internal error", "Unable to log internal fault due to email failure, please contact administrator" );
-    else
-      error("Email sender", "Email to ".$to." could not be sent");
+  
+  if($USE_EMAIL == "N" ) {
+    //email is turned off in config file
+    return;
   }
+    
+  switch($MAIL_METHOD ) {
+  
+    case "mail":
+	    //send message using the standard php mail() function over local sockets 
+        $additional_headers = "From: ".$EMAIL_FROM."\r\n".
+  	  					"Reply-To: ".$EMAIL_REPLY_TO."\r\n".
+						"X-Mailer: PHP/" . phpversion()."\r\n".
+						"X-Priority: 3\r\n".
+						"X-Sender: ".$EMAIL_REPLY_TO."\r\n".
+						"Return-Path: <".$EMAIL_REPLY_TO.">\r\n".
+						"Mime-Version: 1.0\r\n".
+						"Content-Type: text/plain; charset=us-ascii\r\n".
+						"Content-Transfer-Encoding: 7 bit\r\n";
+
+      if( ! mail( $to, $subject, $message, $additional_headers ) )
+        debug("Email to ".to." could not be sent" );
+      break;   
+
+    case "SMTP":
+      //send message using SMTP over local sockets or remote connection
+      $address_list = explode( ",", $to ); 
+      foreach($address_list as $email_to) { 
+
+        //open an SMTP connection at the mail host
+        $host = $SMTP_HOST;
+        $connection = fsockopen ($host, 25, &$errno, &$errstr, 10);
+        if (!$connection)
+          debug("Unable to open SMTP connection to ".$host."<BR><BR>Error ".$errno." ".$errstr );
+
+        //sometimes the SMTP server takes a little longer to respond
+        // Windows still does not have support for this timeout function
+        if(substr(PHP_OS, 0, 3) != "WIN")
+          socket_set_timeout($connection, 1, 0);
+ 
+        $res = fgets($connection, 256 );
+        if(substr($res,0,3) != "220" )
+          debug("Incorrect handshaking response from SMTP server at ".$host."<BR><BR>Response from SMTP server was ".$res );
+
+        //send HELO to server
+        $domain = $DOMAIN;
+        fputs($connection, "HELO $domain\r\n" );
+        $res = fgets($connection, 256 );
+        if(substr($res,0,3) != "250" )
+          debug("Incorrect HELO response from SMTP server at ".$host."<BR><BR>Response from SMTP server was ".$res );
+
+        //evelope from
+        fputs($connection, "MAIL FROM: $EMAIL_FROM\r\n" );
+        $res=fgets($connection, 256 );
+        if(substr($res,0,3) != "250" )
+          debug("Incorrect response to MAIL FROM command from SMTP server at ".$host."<BR><BR>Response from SMTP server was ".$res );
+
+        //envelope to
+        fputs($connection, "RCPT TO: $email_to\r\n" );
+        $res=fgets($connection, 256 );
+        if( (substr($res,0,3) != "250" )  && (substr($res,0,3) != "251" ) )
+          debug("Incorrect response to RCPT TO command from SMTP server at ".$host."<BR><BR>Response from SMTP server was ".$res );
+
+        //message
+        fputs($connection, "DATA\r\n" );
+        $res=fgets($connection, 256 );
+        if(substr($res,0,3) != "354")
+          debug("Incorrect response to DATA command from SMTP server at ".$host."<BR><BR>Response from SMTP server was ".$res );
+
+        $headers = "To: ".$email_to."\r\n".
+ 			"From: ".$EMAIL_FROM."\r\n".
+  			"Reply-To: ".$EMAIL_REPLY_TO."\r\n".
+			"Subject: ".$subject."\r\n".
+			"X-Mailer: PHP/" . phpversion()."\r\n".
+			"X-Priority: 3\r\n".
+			"X-Sender: ".$EMAIL_REPLY_TO."\r\n".
+			"Return-Path: <".$EMAIL_REPLY_TO.">\r\n".
+			"Mime-Version: 1.0\r\n".
+			"Content-Type: text/plain; charset=us-ascii\r\n".
+			"Content-Transfer-Encoding: 7 bit\r\n";
+  
+        //send To:, From:, Subject:, other headers, blank line, message (max 998 bytes per line), and finish
+        // with a period on its own line.
+        fputs($connection, $headers."\r\n".$message."\r\n.\r\n" );
+        $res=fgets($connection, 256 );
+        if(substr($res,0,3) != "250" )
+          debug("Error sending data<BR><BR>Response from SMTP server  at ".$host."was ".$res );
+
+        //say bye bye
+        fputs($connection, "QUIT\r\n" );
+        $res=fgets($connection, 256 );
+        if(substr($res,0,3) != "221" )
+        debug("Incorrect response to QUIT request from SMTP server at ".$host."<BR><BR>Response from SMTP server was ".$res );
+
+        fclose ($connection );
+      }
+      break;
+
+    default:
+      warning("Configuration error", "The email transport type ".$MAIL_METHOD." in the configuration file is not a valid type." ); 
+      break;
+    
+    }
   return;
 }
-
+ 
+ function debug($error_msg ){
+ 
+   global $DEBUG;
+ 
+   if( $DEBUG == "Y" ) {
+      //we don't use error() because email may not work!
+     warning("Email error debug", $error_msg );
+   }
+   else{
+     warning("Internal email fault", "Unable to successfully send your email.  Please contact your administrator" );
+   }
+   return;
+ }
 ?>
