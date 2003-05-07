1d1cfebd21786addc8bd18b1d1c6044a40ae28d5
andrewsimpson <andrew.simpson@paradise.net.nz>
2003-05-07 07:37:35 +0000

    General improvements Support for SMTP AUTH

--- webcollab/includes/email.php
+++ webcollab/includes/email.php
@@ -4,7 +4,7 @@
 
   WebCollab
   ---------------------------------------
-  
+
   This file wriiten 2003 by Andrew Simpson <andrew.simpson@paradise.net.nz>
 
   This program is free software; you can redistribute it and/or modify it under the
@@ -18,16 +18,17 @@
   You should have received a copy of the GNU General Public License along with this
   program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave,
   Cambridge, MA 02139, USA.
-  
+
   Function:
   ---------
 
   Sends emails and preformatted emails
-  
+
   ** Parts of this code lifted from phpmailer (Chris Ryan) and from contributed notes in PHP Manual under fsockopen.
-  
-  Refer to RFC 821 and RFC 2045 for SMTP 
-  
+
+  Refer to RFC 821 and RFC 2045 for SMTP.
+  Refer to RFC 2554 for SMTP AUTH
+
 */
 
 //get our location
@@ -39,22 +40,22 @@ include_once( BASE."includes/admin_config.php" );
 
 function email( $to, $subject, $message) {
 
-  global $EMAIL_FROM, $EMAIL_REPLY_TO, $USE_EMAIL, $MAIL_METHOD, $SMTP_HOST, $DOMAIN;
+  global $EMAIL_FROM, $EMAIL_REPLY_TO, $USE_EMAIL, $MAIL_METHOD, $SMTP_HOST, $DOMAIN, $SMTP_AUTH, $MAIL_USER, $MAIL_PASSWORD;
 
   if( ! valid_string($to) ) {
     //no email address specified - end function
     return;
   }
-  
+
   if($USE_EMAIL == "N" ) {
     //email is turned off in config file
     return;
   }
-    
+
   switch($MAIL_METHOD ) {
-  
+
     case "mail":
-	    //send message using the standard php mail() function over local sockets 
+	    //send message using the standard php mail() function over local sockets
         $additional_headers = "From: ".$EMAIL_FROM."\r\n".
   	  					"Reply-To: ".$EMAIL_REPLY_TO."\r\n".
 						"X-Mailer: PHP/" . phpversion()."\r\n".
@@ -67,12 +68,12 @@ function email( $to, $subject, $message) {
 
       if( ! mail( $to, $subject, $message, $additional_headers ) )
         debug("Email to ".to." could not be sent" );
-      break;   
+      break;
 
     case "SMTP":
       //send message using SMTP over local sockets or remote connection
-      $address_list = explode( ",", $to ); 
-      foreach($address_list as $email_to) { 
+      $address_list = explode( ",", $to );
+      foreach($address_list as $email_to) {
 
         //open an SMTP connection at the mail host
         $host = $SMTP_HOST;
@@ -84,7 +85,7 @@ function email( $to, $subject, $message) {
         // Windows still does not have support for this timeout function
         if(substr(PHP_OS, 0, 3) != "WIN")
           socket_set_timeout($connection, 1, 0);
- 
+
         $res = fgets($connection, 256 );
         if(substr($res,0,3) != "220" )
           debug("Incorrect handshaking response from SMTP server at ".$host."<BR><BR>Response from SMTP server was ".$res );
@@ -95,6 +96,26 @@ function email( $to, $subject, $message) {
         $res = fgets($connection, 256 );
         if(substr($res,0,3) != "250" )
           debug("Incorrect HELO response from SMTP server at ".$host."<BR><BR>Response from SMTP server was ".$res );
+
+        //do SMTP AUTH if required
+        if($SMTP_AUTH == "Y" ) {
+          fputs($connection, "AUTH LOGIN\r\n" );
+          $res = fgets($connection, 256 );
+          if(substr($res,0,3) != "334" )
+            debug("AUTH not accepted by SMTP server at ".$host."<BR><BR>Response from SMTP server was ".$res );
+
+          //send username
+          fputs($connection, base64_encode($MAIL_USER)."\r\n" );
+          $res=fgets($connection, 256 );
+          if(substr($res,0,3) != "334" )
+            debug("Username not accepted SMTP server at ".$host."<BR><BR>Response from SMTP server was ".$res );
+
+          //send password
+          fputs($connection, base64_encode($MAIL_PASSWORD)."\r\n" );
+          $res=fgets($connection, 256 );
+          if(substr($res,0,3) != "334" )
+            debug("Password not accepted SMTP server at ".$host."<BR><BR>Response from SMTP server was ".$res );
+        }
 
         //evelope from
         fputs($connection, "MAIL FROM: $EMAIL_FROM\r\n" );
@@ -124,7 +145,8 @@ function email( $to, $subject, $message) {
           debug("Incorrect response to DATA command from SMTP server at ".$host."<BR><BR>Response from SMTP server was ".$res );
 
 		//generate unique message id
-		$uniq_id = md5(uniqid(time()));
+		mt_srand(time());
+		$uniq_id = md5(uniqid(mt_rand()));
 
 		$headers = "To: ".$email_to."\r\n".
  			"From: ".$EMAIL_FROM."\r\n".
@@ -139,7 +161,7 @@ function email( $to, $subject, $message) {
 			"Mime-Version: 1.0\r\n".
 			"Content-Type: text/plain; charset=us-ascii\r\n".
 			"Content-Transfer-Encoding: 7 bit\r\n";
-  
+
         //send To:, From:, Subject:, other headers, blank line, message (max 998 bytes per line), and finish
         // with a period on its own line.
         fputs($connection, $headers."\r\n".$message."\r\n.\r\n" );
@@ -158,17 +180,17 @@ function email( $to, $subject, $message) {
       break;
 
     default:
-      warning("Configuration error", "The email transport type ".$MAIL_METHOD." in the configuration file is not a valid type." ); 
+      warning("Configuration error", "The email transport type ".$MAIL_METHOD." in the configuration file is not a valid type." );
       break;
-    
+
     }
   return;
 }
- 
+
  function debug($error_msg ){
- 
+
    global $DEBUG;
- 
+
    if( $DEBUG == "Y" ) {
       //we don't use error() because email may not work!
      warning("Email error debug", $error_msg );
