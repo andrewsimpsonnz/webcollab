e2cd89a4b5dc8e039d2f4ce5d26afd96c96f69aa
andrewsimpson <andrew.simpson@paradise.net.nz>
2003-08-23 04:53:19 +0000

    Remove php mail() function - was buggy on some platforms.  Make SMTP mail more robust and RFC compliant.

--- webcollab/includes/email.php
+++ webcollab/includes/email.php
@@ -26,7 +26,7 @@
 
   ** Parts of this code lifted from phpmailer (Chris Ryan) and from contributed notes in PHP Manual under fsockopen.
 
-  Refer to RFC 821 and RFC 2045 for SMTP.
+  Refer to RFC 821, RFC 822 and RFC 2045 for SMTP.
   Refer to RFC 2554 for SMTP AUTH
 
 */
@@ -59,149 +59,187 @@ return $text;
 
 function email($to, $subject, $message ) {
 
-  global $EMAIL_FROM, $EMAIL_REPLY_TO, $USE_EMAIL, $MAIL_METHOD, $SMTP_HOST, $SMTP_AUTH, $MAIL_USER, $MAIL_PASSWORD;
-
-  if( ! valid_string($to) ) {
-    //no email address specified - end function
-    return;
-  }
+  global $EMAIL_FROM, $EMAIL_REPLY_TO, $USE_EMAIL, $SMTP_HOST, $SMTP_AUTH, $MAIL_USER, $MAIL_PASSWORD;
 
   if($USE_EMAIL == "N" ) {
     //email is turned off in config file
     return;
   }
 
-  switch($MAIL_METHOD ) {
+  if( ! valid_string($to) ) {
+    //no email address specified - end function
+    return;
+  }
 
-    case "mail":
-        //send message using the standard php mail() function over local sockets
-        $additional_headers = "From: ".$EMAIL_FROM."\r\n".
-                            "Reply-To: ".$EMAIL_REPLY_TO."\r\n".
-                            "X-Mailer: PHP/" . phpversion()."\r\n".
-                            "X-Priority: 3\r\n".
-                            "X-Sender: ".$EMAIL_REPLY_TO."\r\n".
-                            "Return-Path: <".$EMAIL_REPLY_TO.">\r\n".
-                            "Mime-Version: 1.0\r\n".
-                            "Content-Type: text/plain; charset=us-ascii\r\n".
-                            "Content-Transfer-Encoding: 7 bit\r\n";
+  //send message using SMTP
+  //open an SMTP connection at the mail host
+  $host = $SMTP_HOST;
+  $connection = fsockopen ($host, 25, &$errno, &$errstr, 10 );
+  if (!$connection )
+    debug("Unable to open SMTP connection to ".$host."<br /><br />Error ".$errno." ".$errstr );
 
-      if( ! mail($to, $subject, $message, $additional_headers ) )
-        debug("Email to $to could not be sent" );
-      break;
+  //sometimes the SMTP server takes a little longer to respond
+  // Windows does not have support for this timeout function before PHP ver 4.3.0
+  if(substr(PHP_OS, 0, 3) != "WIN" )
+    socket_set_timeout($connection, 1, 0 );
 
-    case "SMTP":
-      //send message using SMTP over local sockets or remote connection
-      $address_list = explode(",", $to );
-      foreach($address_list as $email_to ) {
+  $res = fgets($connection, 256 );
+  if(substr($res, 0, 3 ) != "220" )
+    debug("Incorrect handshaking response from SMTP server at $host <br /><br />Response from SMTP server was $res" );
 
-        //open an SMTP connection at the mail host
-        $host = $SMTP_HOST;
-        $connection = fsockopen ($host, 25, &$errno, &$errstr, 10 );
-        if (!$connection )
-          debug("Unable to open SMTP connection to ".$host."<br /><br />Error ".$errno." ".$errstr );
+  //send HELO to server
+  fputs($connection, "HELO ".$_SERVER["SERVER_NAME"]."\r\n" );
+  $res = fgets($connection, 256 );
+  if(substr($res, 0, 3 ) != "250" )
+    debug("Incorrect HELO response from SMTP server at $host <br /><br />Response from SMTP server was $res" );
 
-        //sometimes the SMTP server takes a little longer to respond
-        // Windows still does not have support for this timeout function
-        if(substr(PHP_OS, 0, 3) != "WIN" )
-          socket_set_timeout($connection, 1, 0 );
+  //do SMTP AUTH if required
+  if($SMTP_AUTH == "Y" ) {
+    fputs($connection, "AUTH LOGIN\r\n" );
+    $res = fgets($connection, 256 );
+    if(substr($res, 0, 3 ) != "334" )
+        debug("AUTH not accepted by SMTP server at $host <br /><br />Response from SMTP server was $res" );
 
-        $res = fgets($connection, 256 );
-        if(substr($res, 0, 3 ) != "220" )
-          debug("Incorrect handshaking response from SMTP server at $host <br /><br />Response from SMTP server was $res" );
+    //send username
+    fputs($connection, base64_encode($MAIL_USER )."\r\n" );
+    $res = fgets($connection, 256 );
+    if(substr($res, 0, 3 ) != "334" )
+        debug("Username not accepted SMTP server at $host <br /><br />Response from SMTP server was $res" );
 
-        //send HELO to server
-        fputs($connection, "HELO ".$_SERVER["SERVER_NAME"]."\r\n" );
-        $res = fgets($connection, 256 );
-        if(substr($res, 0, 3 ) != "250" )
-          debug("Incorrect HELO response from SMTP server at $host <br /><br />Response from SMTP server was $res" );
+    //send password
+    fputs($connection, base64_encode($MAIL_PASSWORD )."\r\n" );
+    $res = fgets($connection, 256 );
+    if(substr($res, 0, 3 ) != "334" )
+        debug("Password not accepted SMTP server at $host <br /><br />Response from SMTP server was $res" );
+  }
 
-        //do SMTP AUTH if required
-        if($SMTP_AUTH == "Y" ) {
-          fputs($connection, "AUTH LOGIN\r\n" );
-          $res = fgets($connection, 256 );
-          if(substr($res, 0, 3 ) != "334" )
-            debug("AUTH not accepted by SMTP server at $host <br /><br />Response from SMTP server was $res" );
+  //evelope from
+  fputs($connection, "MAIL FROM: <$EMAIL_FROM>\r\n" );
+  $res = fgets($connection, 256 );
+  if(substr($res, 0, 3 ) != "250" )
+    debug("Incorrect response to MAIL FROM command from SMTP server at $host <br /><br />Response from SMTP server was $res" );
 
-          //send username
-          fputs($connection, base64_encode($MAIL_USER )."\r\n" );
-          $res=fgets($connection, 256 );
-          if(substr($res, 0, 3 ) != "334" )
-            debug("Username not accepted SMTP server at $host <br /><br />Response from SMTP server was $res" );
+  //envelope to
+  $address_list = explode(",", $to );
+  foreach($address_list as $email_to ) {
+    fputs($connection, "RCPT TO: <$email_to>\r\n" );
+    $res = fgets($connection, 256 );
 
-          //send password
-          fputs($connection, base64_encode($MAIL_PASSWORD )."\r\n" );
-          $res=fgets($connection, 256 );
-          if(substr($res, 0, 3 ) != "334" )
-            debug("Password not accepted SMTP server at $host <br /><br />Response from SMTP server was $res" );
-        }
+    switch(substr($res, 0, 3 ) ) {
+      case "250":
+      case "251":
+        //acceptable responses
+        break;
 
-        //evelope from
-        fputs($connection, "MAIL FROM: $EMAIL_FROM\r\n" );
-        $res=fgets($connection, 256 );
-        if(substr($res, 0, 3 ) != "250" )
-          debug("Incorrect response to MAIL FROM command from SMTP server at $host <br /><br />Response from SMTP server was $res" );
+      case "450":
+      case "550":
+        //mail box error
+        debug("Mailbox error for $email_to <br /><br />Response from SMTP server was $res" );
+        break;
 
-        //envelope to
-        fputs($connection, "RCPT TO: $email_to\r\n" );
-        $res=fgets($connection, 256 );
-
-        switch(substr($res, 0, 3 ) ) {
-          case "250":
-          case "251":
-            //acceptable responses
-            break;
-
-          default:
-            debug("Incorrect response to RCPT TO command from SMTP server at $host <br /><br />Response from SMTP server was $res" );
-            break;
-        }
-
-        //message
-        fputs($connection, "DATA\r\n" );
-        $res=fgets($connection, 256 );
-        if(substr($res, 0, 3 ) != "354")
-          debug("Incorrect response to DATA command from SMTP server at $host <br /><br />Response from SMTP server was $res" );
-
-        //generate unique message id
-        mt_srand(time());
-        $uniq_id = md5(uniqid(mt_rand()));
-
-        $headers = "Date: ".date("r")."\r\n".
-            "To: ".$email_to."\r\n".
-            "From: ".$EMAIL_FROM."\r\n".
-            "Reply-To: ".$EMAIL_REPLY_TO."\r\n".
-            "Subject: ".$subject."\r\n".
-            "Message-Id: <".$uniq_id."@".$_SERVER["SERVER_NAME"].">\r\n".
-            "X-Mailer: PHP/" . phpversion()."\r\n".
-            "X-Priority: 3\r\n".
-            "X-Sender: ".$EMAIL_REPLY_TO."\r\n".
-            "Return-Path: <".$EMAIL_REPLY_TO.">\r\n".
-            "Mime-Version: 1.0\r\n".
-            "Content-Type: text/plain; charset=us-ascii\r\n".
-            "Content-Transfer-Encoding: 7 bit\r\n";
-
-        //send To:, From:, Subject:, other headers, blank line, message (max 998 bytes per line), and finish
-        // with a period on its own line.
-        fputs($connection, $headers."\r\n".$message."\r\n.\r\n" );
-        $res=fgets($connection, 256 );
-        if(substr($res, 0, 3 ) != "250" )
-          debug("Error sending data<br /><br />Response from SMTP server  at $host was $res" );
-
-        //say bye bye
-        fputs($connection, "QUIT\r\n" );
-        $res=fgets($connection, 256 );
-        if(substr($res, 0, 3 ) != "221" )
-        debug("Incorrect response to QUIT request from SMTP server at $host <br /><br />Response from SMTP server was $res" );
-
-        fclose ($connection );
-      }
-      break;
-
-    default:
-      warning("Configuration error", "The email transport type $MAIL_METHOD in the configuration file is not a valid type." );
-      break;
-
+      default:
+        //anything else is no good
+        debug("Incorrect response to RCPT TO: $email_to from SMTP server at $host <br /><br />Response from SMTP server was $res" );
+        break;
     }
+  }
+
+  //start data transmission
+  fputs($connection, "DATA\r\n" );
+  $res = fgets($connection, 256 );
+  if(substr($res, 0, 3 ) != "354")
+    debug("Incorrect response to DATA command from SMTP server at $host <br /><br />Response from SMTP server was $res" );
+
+  //send the date - in RFC 822 format
+  fputs($connection, "Date: ".date("r")."\r\n" );
+
+  //now the prepare the 'to' header
+
+  //RFC 821 requires that headers longer than 998 characters are broken up to separate lines
+  //(end long line with \r\n, and begin new line with \t)
+  $line = "To: ".$to;
+  $lines_out = "";
+  //check if we need to break up into several smaller lines
+  while(strlen($line ) > 998 ) {
+    $pos = strrpos(substr($line, 0, 998 ), " " );
+    $lines_out[] = substr($line, 0, $pos );
+    $line = "\t".substr($line, $pos + 1 );
+  }
+  $lines_out[] = $line;
+
+  //now send the "to" lines to the server
+  while(list(,$line_out) = @each($lines_out ) ) {
+    fputs($connection, "$line_out\r\n" );
+  }
+
+  //generate unique message id
+  mt_srand(time());
+  $uniq_id = md5(uniqid(mt_rand()));
+
+  //we can reasonably assume that these headers will always be less than 998 characters per line
+  //(subject line is truncated to be sure)
+  $headers = array("From: ".$EMAIL_FROM."\r\n",
+                        "Reply-To: ".$EMAIL_REPLY_TO."\r\n",
+                        "Subject: ".substr($subject, 0, 985 )."\r\n",
+                        "Message-Id: <".$uniq_id."@".$_SERVER["SERVER_NAME"].">\r\n",
+                        "X-Mailer: PHP/" . phpversion()."\r\n",
+                        "X-Priority: 3\r\n",
+                        "X-Sender: ".$EMAIL_REPLY_TO."\r\n",
+                        "Return-Path: <".$EMAIL_REPLY_TO.">\r\n",
+                        "Mime-Version: 1.0\r\n",
+                        "Content-Type: text/plain; charset=us-ascii\r\n",
+                        "Content-Transfer-Encoding: 7 bit\r\n \r\n");
+
+  //send headers line by line
+  for( $i=0; $i < 11; $i++ ) {
+    fputs($connection, $headers[$i] );
+  }
+
+  // according to rfc 821 we should not send more than 998 characters on a single line
+  // thanks to phpmailer for this code...
+
+  //normalise the line breaks so we know the explode works
+  $message = str_replace("\r\n", "\n", $message );
+  $message = str_replace("\r", "\n", $message );
+  $all_lines = explode("\n", $message );
+
+  while(list(,$line ) = @each($all_lines ) ) {
+    $lines_out = "";
+    //check if we need to break this line up into several smaller lines
+    while(strlen($line) > 998 ) {
+      $pos = strrpos(substr($line, 0, 998 ), " " );
+      $lines_out[] = substr($line, 0, $pos );
+      $line = substr($line, $pos + 1 );
+    }
+    $lines_out[] = $line;
+
+    //now send the lines to the server
+    while(list(,$line_out) = @each($lines_out ) ) {
+      if(strlen($line_out) > 0 ){
+        // for lines starting with . add another .
+        if(substr($line_out, 0, 1 ) == "." ) {
+          $line_out = "." . $line_out;
+        }
+      }
+      fputs($connection, "$line_out\r\n" );
+    }
+  }
+
+  // ok all the message data has been sent - finish with a period on it's own line
+  fputs($connection, "\r\n.\r\n" );
+  $res = fgets($connection, 256 );
+  if(substr($res, 0, 3 ) != "250" )
+    debug("Error sending data<br /><br />Response from SMTP server  at $host was $res" );
+
+  //say bye bye
+  fputs($connection, "QUIT\r\n" );
+  $res = fgets($connection, 256 );
+  if(substr($res, 0, 3 ) != "221" )
+    debug("Incorrect response to QUIT request from SMTP server at $host <br /><br />Response from SMTP server was $res" );
+
+  fclose ($connection );
+
+
   return;
 }
 
