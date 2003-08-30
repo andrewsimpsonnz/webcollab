7a13ac46131a6aaece7dd8553f0be87e5e3bd821
andrewsimpson <andrew.simpson@paradise.net.nz>
2003-08-30 20:28:14 +0000

    Match to new stylesheet. TODO: Put center into stylesheet? More tuning.

--- webcollab/index.php
+++ webcollab/index.php
@@ -162,7 +162,7 @@ $content .= "<br />".$lang["please_login"].":<br /><br />\n".
            "<br />";
 
 //set box options
-new_box($lang["login"], $content, "400" );
+new_box($lang["login"], $content, "boxdata", "loginbox" );
 
 create_bottom();
 
