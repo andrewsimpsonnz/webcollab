25c79878717de7511590a61f8c62ee070c8c0ab5
andrewsimpson <andrew.simpson@paradise.net.nz>
2003-10-31 08:10:32 +0000

    Update to new format

--- webcollab/lang/ca_email.php
+++ webcollab/lang/ca_email.php
@@ -29,60 +29,43 @@
   Maintainer:
 
 */
+$email_list = "Projecte: %s\n".
+               "Tasca:    %s\n".
+               "Estat:   %s\n".
+               "A c&agrave;rreg:  %s ( %s )\n".
+               "Text:\n%s\n\n".
+               "Dirigir-se al lloc web per a mes detalls.\n\n%s\n";
 
-$title_takeover     = $ABBR_MANAGER_NAME.": El seu item ha estat reassignat";
-$email_takeover     = "Hola,\n\nAquest &eacute;e %s lloc informant-li que un %s al seu c&agrave;rreg ha estat reassignada per l'administrador el %s.\n\n".
-			"Projecte: %s\n".
-			"Tasca:    %s\n".
-                        "A c&agrave;rreg:  %s ( %s )\n".
-			"Text:\n%s\n\n".
-			"Dirigir-se al lloc web per a mes detalls.\n\n%s\n";
+$title_takeover_project   = $ABBR_MANAGER_NAME.": El seu item ha estat reassignat";
+$title_takeover_task      = $ABBR_MANAGER_NAME.": El seu item ha estat reassignat";
+$email_takeover_project   = "Hola,\n\nAquest &eacute;e %s lloc informant-li que un projecte al seu c&agrave;rreg ha estat reassignada per l'administrador el %s.\n\n";
+$email_takeover_task      = "Hola,\n\nAquest &eacute;e %s lloc informant-li que un tasca al seu c&agrave;rreg ha estat reassignada per l'administrador el %s.\n\n";
 
-$title_new_owner    = $ABBR_MANAGER_NAME.": Nou %s per a vost&eacute;";
-$email_new_owner    = "Hola,\n\nAquest es el %s lloc informant-li que un %s seu (ara al seu c&agrave;rreg) va ser canviat el %s.\n\nAqu&iacute; els detalls:\n\n".
-			"Projecte: %s\n".
-			"Tasca:    %s\n".
-			"Estat:   %s\n\n".
-			"Text:\n%s\n\n".
-                        "Dirigir-se al lloc web per a m&eacute; detalls.\n\n%s\n";
-
-
-$title_new_group    = $ABBR_MANAGER_NAME.": Nou %s: ";
-$email_new_group    = "Hola,\n\nAquest &eacute;s el %s lloc informant-li que un nou %s ha estat creat el %s\n\nAqu&iacute; els detalls:".
-			"Projecte: %s\n".
-			"Tasca:    %s\n".
-                        "A c&agrave;rreg:  %s\n".
-			"Estat:   %s\n\n".
-			"Text:\n%s\n\n".
-                        "Dirigir-se al lloc web per a m&eacute;s detall.\n\n%s\n";
-
-$title_edit_owner   = $ABBR_MANAGER_NAME.": La seva %s actualitzada";
-$email_edit_owner   ="Hola,\n\nAquest &eacute;s el %s lloc informant-li que un %s al seu c&agrave;rreg va canviar el %s.\n\nAqu&iacute; els detalls:\n\n".
-                        "Projecte: %s\n".
-                        "Tasca:    %s\n".
-                        "Estat:   %s\n\n".
-                        "Text:\n%s\n\n".
-                        "Dirigir-se al lloc web per a m&eacute;s detalls.\n\n%s\n";
+$title_new_owner_project  = $ABBR_MANAGER_NAME.": Nou projecte per a vost&eacute;";
+$title_new_owner_task     = $ABBR_MANAGER_NAME.": Nou tasca per a vost&eacute;";
+$email_new_owner          = "Hola,\n\nAquest es el %s lloc informant-li que un projecte seu (ara al seu c&agrave;rreg) va ser canviat el %s.\n\nAqu&iacute; els detalls:\n\n";
+$email_new_owner          = "Hola,\n\nAquest es el %s lloc informant-li que un tasca seu (ara al seu c&agrave;rreg) va ser canviat el %s.\n\nAqu&iacute; els detalls:\n\n";
 
 
-$title_edit_group   = $ABBR_MANAGER_NAME.": %s actualitzada";
-$email_edit_group   = "Hola,\n\nAquest &eacute;s el %s lloc informant-li que un %s a c&agrave;rreg de %s ha canviat el %s.\n\nAquí els detalls:\n\n".
-			"Projecte: %s\n".
-			"Tasca:    %s\n".
-			"Estat:   %s\n\n".
-			"Text:\n%s\n\n".
-			"Dirigir-se al lloc web per a més detalls.\n\n%s\n";
+$title_new_group_project  = $ABBR_MANAGER_NAME.": Nou projecte: %s";
+$title_new_group_task     = $ABBR_MANAGER_NAME.": Nou tasca: %s";
+$email_new_group_project  = "Hola,\n\nAquest &eacute;s el %s lloc informant-li que un nou projecte ha estat creat el %s\n\nAqu&iacute; els detalls:";
+$email_new_group_task     = "Hola,\n\nAquest &eacute;s el %s lloc informant-li que un nou tasca ha estat creat el %s\n\nAqu&iacute; els detalls:";
 
+$title_edit_owner_project = $ABBR_MANAGER_NAME.": La seva projecte actualitzada";
+$title_edit_owner_task    = $ABBR_MANAGER_NAME.": La seva tasca actualitzada";
+$email_edit_owner_project ="Hola,\n\nAquest &eacute;s el %s lloc informant-li que un projecte al seu c&agrave;rreg va canviar el %s.\n\nAqu&iacute; els detalls:\n\n";
+$email_edit_owner_task    ="Hola,\n\nAquest &eacute;s el %s lloc informant-li que un tasca al seu c&agrave;rreg va canviar el %s.\n\nAqu&iacute; els detalls:\n\n";
 
-$title_delete_task  = $ABBR_MANAGER_NAME.": %s eliminada";
-$email_delete_task  = "Hola,\n\n".
-			"Aquest &eacute;s el %s lloc informant-li que un %s al seu c&agrave;rreg ha estat eliminat el %s\n\n".
-			"Projecte: %s\n".
-			"Tasca:    %s\n".
-			"Estat:   %s\n\n".
-			"Text:\n%s\n\n".
-			"Gr&agrave;cies per dirigir la tasca al seu moment.";
+$title_edit_group_project = $ABBR_MANAGER_NAME.": Projecte actualitzada";
+$title_edit_group_task    = $ABBR_MANAGER_NAME.": Tasca actualitzada";
+$email_edit_group_project = "Hola,\n\nAquest &eacute;s el %s lloc informant-li que un projecte a c&agrave;rreg de %s ha canviat el %s.\n\nAqu&iacute; els detalls:\n\n";
+$email_edit_group_task    = "Hola,\n\nAquest &eacute;s el %s lloc informant-li que un tasca a c&agrave;rreg de %s ha canviat el %s.\n\nAqu&iacute; els detalls:\n\n";
 
+$title_delete_project     = $ABBR_MANAGER_NAME.": Projecte eliminada";
+$title_delete_task        = $ABBR_MANAGER_NAME.": Tasca eliminada";
+$email_delete_project     = "Hola,\n\nAquest &eacute;s el %s lloc informant-li que un projecte al seu c&agrave;rreg ha estat eliminat el %s\n\n";
+$email_delete_task        = "Hola,\n\nAquest &eacute;s el %s lloc informant-li que un tasca al seu c&agrave;rreg ha estat eliminat el %s\n\n";
 
 $title_welcome      = "Benvinguda a ".$ABBR_MANAGER_NAME;
 $email_welcome      = "Hola,\n\nAquest &eacute;s el lloc %s donant-li la benvinguda ;) el  %s.\n\n".
@@ -92,7 +75,7 @@ $email_welcome      = "Hola,\n\nAquest &eacute;s el lloc %s donant-li la benving
 			"Cada &iacute;tem que vost&egrave; envia o tasca que editar ser&agrave; mostrada als altres usuaris com 'nova' o 'actualitzada'. Aix&ograve; tamb&eacute; funciona a la inversa i ".
 			"el permet r&agrave;pidamentlo enfocar on est&agrave; l'activitat.\n\n".
 			"Vost&eacute; pot tamb&eacute; fer-se c&agrave;rreg o prendre propietat de tasques i es trobar&agrave; habilitat per editar i tots els enviaments al f&ograve;rum seran rebuts. ".
-			"A mesura que avança en la seva feina, per favor editi el text de les seves tasques i l'estat de tal forma que tothom pugui mantenir un seguiment del seu progr&eagrave;s. ".
+			"A mesura que avan? en la seva feina, per favor editi el text de les seves tasques i l'estat de tal forma que tothom pugui mantenir un seguiment del seu progr&eagrave;s. ".
 			"\n\nNo em resta m&eacute;s que desijar-li &egrave;xits i informar-li que pot enviar un correu a %s si es troba amb alguna dificultat.\n\n --Bona sort !\n\n".
 			"Usuari:      %s\n".
 			"Clau:   %s\n\n".
