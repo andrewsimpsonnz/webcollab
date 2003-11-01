e79cf3968aaed278b644d0d62f27d8071d0156e8
andrewsimpson <andrew.simpson@paradise.net.nz>
2003-11-01 22:36:04 +0000

    Change language string for better internationalisation

--- webcollab/tasks/task_project_list.php
+++ webcollab/tasks/task_project_list.php
@@ -182,7 +182,7 @@ for( $i=0 ; $row = @db_fetch_array($q, $i ) ; $i++) {
       break;
 
     case "cantcomplete":
-      $content .= "<i>".$lang["project_hold"].nicedate( $row["finished_time"])."</i><br />\n";
+      $content .= "<i>".sprintf($lang["project_hold_sprt"], nicedate($row["finished_time"]) )."</i><br />\n";
       $content .= "<img border=\"0\" src=\"images/clock.gif\" height=\"9\" width=\"9\" alt=\"clock\" /> &nbsp; ".nicedate( $row["deadline"] )."<br />\n";
       break;
 
