a5b00f4cbda177e9c151c096e34f36eb8afa648a
andrewsimpson <andrew.simpson@paradise.net.nz>
2003-12-10 06:10:09 +0000

    Formatting

--- webcollab/contacts/contact_show.php
+++ webcollab/contacts/contact_show.php
@@ -61,7 +61,7 @@ $content .=
        "<tr><td><i>".$lang["city"]."</i></td><td>".$row["city"]."</td></tr>\n".
        "<tr><td><i>".$lang["email"]."</i></td><td><a href=\"mailto:".$row["email"]."\">".$row["email"]."</a></td></tr>\n".
     "</table><br />\n".
-    "<i>".$lang["notes"]."</i><br />".nl2br($row["notes"] )."<br />\n".
+    "<p><i>".$lang["notes"]."</i><br />".nl2br($row["notes"] )."</p>\n".
     "<form method=\"POST\" action=\"contacts.php\" />\n".
       "<input type=\"hidden\" name=\"action\" value=\"edit\" />\n".
       "<input type=\"hidden\" name=\"contactid\" value=\"".$row["id"]."\" />\n".
