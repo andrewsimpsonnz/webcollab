047c67c48effd3f11e3b281ac06e0848846eaa3d
andrewsimpson <andrew.simpson@paradise.net.nz>
2005-02-16 07:32:17 +0000

    Initial upload

---
+++ webcollab/lang/pt-br_long_message.php
@@ -0,0 +1,63 @@
+<?php
+/*
+  $Id$
+
+  WebCollab
+  ---------------------------------------
+  This file created 2003 by Andrew Simpson
+  
+  This program is free software; you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software Foundation;
+  either version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with this
+  program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+
+
+  Function:
+  ---------
+
+  Email text language files for 'pt_br' (Brazilian Portuguese)
+
+  Translation: Eduardo Alvim <eduardoalvim at terra.com.br>
+
+*/
+
+$taskgroup_info =   "<ul><li>Se voc&ecirc; excluir um grupo de tarefas, todas as tarefas pertencentes a ele se tornar&atilde;o n&atilde;o-categorizadas..</li>\n".
+                      "<li>Voc&ecirc; pode alterar o nome de uma categoria sem interferir nas tarefas.</li>\n".
+                      "<li>Dois grupos de tarefas n&atilde;o podem ter nomes iguais.</li></ul>\n";
+
+$usergroup_info =   "<ul><li>Se voc&ecirc; excluir um grupo de usu&aacute;rios, todas as mensagens particulares relacionadas a ele existentes nos f&oacute;runs tamb&eacute;m o ser&atilde;o.</li>\n".
+                      "<li>Grupos privativos de usu&aacute;rios s&oacute; podem ser enxergados por seus membros.</li>\n".
+                      "<li>Voc&ecirc; pode alterar o nome de um grupo de usu&aacute;rios sem interferir em seus membros.</li>\n".
+                      "<li>Dois grupos de usu&aacute;rios n&atilde;o podem ter nomes iguais.</li></ul>\n";
+
+$user_info      =    "Por favor, escolha o que fazer usando o menu &agrave; esquerda.<br /><br />".
+                      "Algumas dicas r&aacute;pidas:<br />".
+                      "<ul>".
+                      //**
+                      "<li>Usu&aacute;rios privativos s&oacute; podem ser enxergados por membros do mesmo grupo de usu&aacute;rios.</li>\n".
+                      "<li>A exclus&atilde;o de usu&aacute;rios possui dois est&aacute;gios, sendo o segundo caracterizado por seu car&aacute;ter permanente.</li>\n".
+                      "<li>Um usu&aacute;rio que &eacute; exclu&iacute;do perde todas as suas tarefas, mas mant&eacute;m suas mensagens nos f&oacute;runs.</li>\n".
+                      "<li>Um usu&aacute;rio que &eacute; permanentemente exclu&iacute;do perde tudo.</li>\n".
+                      //"<li>Voc&ecirc; n&atilde;o pode excluir permanentemente um usu&aacute;rio que ainda possua ítens nos fóruns.</li>\n".
+                      "<li>Um usu&aacute;rio exclu&iacute;do mant&eacute;m o registro de suas tarefas j&aacute; realizadas, continuando com esta lista at&eacute; ser ressucitado.</li>\n".
+                      "<li>TODAS as a&ccedil;&otilde;es realizadas por um usu&aacute;rio ser&atilde;o notificadas ao pr&oacute;prio via e-mail.</li>\n".
+                      "<li>As senhas s&atilde;o encriptografadas no banco de dados. Voc&ecirc; s&oacute; pode registrar uma nova.</li>\n".
+                      "<li>As senhas s&oacute; s&atilde;o enviadas via e-mail uma &uacute;nica vez aos usu&aacute;rios, no momento em que s&atilde;o registradas. Portanto as envie para um endere&ccedil;o seguro!</li>\n".
+                      "<li>Os usu&aacute;rios podem se auto-editarem sem o conhecimento da administra&ccedil;&atilde;o - isso ir&aacute; economizar seu tempo</li>\n".
+                      "</ul>\n";
+
+$calendar_key    =  "<i>Retornar ao Menu Principal</i></a>]</b><br />\n".
+                      "<p><b><span class=\"underline\">Legenda para o Calend&aacute;rio</span></b><br /><br />\n".
+                      "<span class=\"blue\">Projeto (com tarefas incompletas)</span><br />\n".
+                      "<span class=\"green\"><span class=\"underline\">Projeto </span>(todas as tarefas completas)</span><br />\n".
+                      "<span class=\"red\">Tarefa (incompleta)</span><br />\n".
+                      "<span class=\"green\">Tarefa (completa)</span><br /></p>\n";
+
+?>
