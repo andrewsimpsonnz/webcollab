083cc54ded8ad5085bbfcd1f2168ce159365a0d3
andrewsimpson <andrew.simpson@paradise.net.nz>
2005-02-28 07:54:46 +0000

    Initial upload

---
+++ webcollab/lang/hu_long_message.php
@@ -0,0 +1,65 @@
+<?php
+/*
+  $Id$
+
+  WebCollab
+  ---------------------------------------
+  This file created 2003 by Andrew Simpson
+  
+  This program is free software; you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software Foundation;
+  either version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with this
+  program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+
+
+  Function:
+  ---------
+
+  Language files (long messages) for 'en' (English)
+
+  Maintainer: Andrew Simpson <andrew.simpson at paradise.net.nz>
+
+*/
+
+
+$taskgroup_info =   "<ul><li>Ha töröl egy feladatcsoportot, akkor minden hozzá tartozó feladat bekategorizálatlanná válik.</li>\n".
+                      "<li>Egy kategória nevét a feladatokkal való összeakadás nélkül megváltoztathatja.</li>\n".
+                      "<li>Két feladatcsoportnak nem lehet ugyanaz a neve.</li></ul>\n";
+
+$usergroup_info =   "<ul><li>Ha töröl egy feladatcsoportot, akkor minden hozzá tartozó privát fórum üzenet is elvész.</li>\n".
+                      "<li>A privát felhasználói csooprtot csak a tagjai láthatják.</li>\n".
+                      "<li>Egy felhasználói csoport nevét a felhsznlókkal való összeakadás nélkül megváltoztathatja.</li>\n".
+                      "<li>Két felhasználói csoportnak nem lehet ugyanaz a neve.</li></ul>\n";
+
+$user_info      =    "Kérem válassza ki az utasítást a baloldali menübõl!<br /><br />".
+                      "Néhány gyors tipp:<br />".
+                      "<ul>".
+                      //**
+                      "<li>A privát felhasználókat csak a felhasználói csoport tagjai láthatják.</li>\n".
+                      "<li>A felhasználók törlésének két fázisa van, a második a végleges.</li>\n".
+                      "<li>Egy töröltfelhasználó elveszti a feladatait, de a fórum üzeneteit nem.</li>\n".
+                      "<li>Egy véglegesen kitörölt felhasználó mindent elveszt.</li>\n".
+                      //"<li>Nem törölhetsz véglegesen olyan felhasználót, amelynek még vannak fórum elemei.</li>\n".
+                      "<li>Egy törölt felhasználó megtartja a látogatott feladatok listáját, és újjáélesztés után ezzel a listával folytatja.</li>\n".
+                      "<li>MINDEN, amit egy felhasználón mûvelnek el lesz küldve emailen a felhasználónak.</li>\n".
+                      "<li>A jelszavak titkosítva vannak az adatbázisban. Csak újat tudsz megadni.</li>\n".
+                      "<li>A jelszavakat csak egyszer, megadásukkor küldjük ki a felhasználónak, úgyhogy figyelemmel küldje el õket!</li>\n".
+                      "<li>A felhasználók szerkeszthetik beállításaikat az admin nélkül - így idõt (és levélszemetet) takarít meg</li>\n".
+                      "</ul>\n";
+
+$calendar_key    =  "<i>Vissza a fõmenübe</i></a>]</b><br />\n".
+                      "<p><b><span class=\"underline\">Kulcs a Naptárhoz</span></b><br /><br />\n".
+                      "<span class=\"blue\">Projekt (befejezetlen feladatokkal)</span><br />\n".
+                      "<span class=\"green\"><span class=\"underline\">Projekt </span>(minden feladat befejezve)</span><br />\n".
+                      "<span class=\"red\">Feladat (nincs befejezve)</span><br />\n".
+                      "<span class=\"green\">Feladat (befejezve)</span><br /></p>\n";
+
+?>
+
