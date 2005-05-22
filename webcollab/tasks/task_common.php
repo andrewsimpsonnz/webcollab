1701a16cd4b314b992135e00868f93dea5e18483
andrewsimpson <andrew.simpson@paradise.net.nz>
2005-05-22 09:30:26 +0000

    Fix typo

--- webcollab/tasks/task_common.php
+++ webcollab/tasks/task_common.php
@@ -65,7 +65,7 @@ function show_percent($percent=0 ) {
     return "<table width=\"400px\"><tr><td style=\"width : 400px\" class=\"redbar\"></td></tr></table>\n";
   }
   if($percent == 100 ){
-    return "<table width=\"400px\"><tr><td style=\"width : 400px\" class=\"redbar\"></td></tr></table>\n";
+    return "<table width=\"400px\"><tr><td style=\"width : 400px\" class=\"greenbar\"></td></tr></table>\n";
   }    
   return "<table width=\"400px\"><tr><td style=\"width\" :".($percent * (400/100))."px class=\"greenbar\">".
            "</td><td style=\"width :".(400-($percent*(400/100)))."px\" class=\"redbar\"></td></tr></table>\n"; 
