40dc75b6e7d6d32ad72c156dcc3e39df7d15b631
andrewsimpson <andrew.simpson@paradise.net.nz>
2005-05-31 05:41:05 +0000

    Initial upload

---
+++ webcollab/users/user_deleted_list.php
@@ -0,0 +1,67 @@
+<?php
+/*
+  $Id$
+  
+  (c) 2002 - 2005 Andrew Simpson <andrew.simpson at paradise.net.nz>
+
+  WebCollab
+  ---------------------------------------
+  Based on CoreAPM by Dennis Fleurbaaij 2001/2002
+
+  This program is free software; you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software Foundation;
+  either version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with this
+  program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+
+  Function:
+  ---------
+
+  Shows the deleted users and allow to revive them
+
+*/
+
+require_once('path.php' );
+require_once(BASE.'includes/security.php' );
+
+//first check if we are admin
+if(! ADMIN ){
+  return;
+}
+
+//check for deleted users
+if( ! db_result(db_query('SELECT COUNT(*) FROM '.PRE.'users WHERE deleted=\'t\'' ), 0, 0 ) ) {
+  $content = "<small>".$lang['no_deleted_users']."</small>";
+  new_box($lang['deleted_users'], $content, "boxdata" );
+  return;
+}
+
+//query
+$q = db_query('SELECT id, fullname FROM '.PRE.'users WHERE deleted=\'t\' ORDER BY fullname' );
+
+$content = "<table>\n";
+
+//show them
+for($i=0 ; $row = @db_fetch_array($q, $i ) ; ++$i ) {
+  $content .= "<tr><td style=\"padding-right: 5px\"><a href=\"users.php?x=$x&amp;action=show&amp;userid=".$row['id']."\">".$row['fullname']."</a></td>\n".
+              "<td style=\"text-align:right; white-space:nowrap\"><span class=\"textlink\">[<a href=\"users.php?x=$x&amp;action=revive&amp;userid=".$row['id']."\">".$lang['revive']."</a>]&nbsp;";
+  
+  //if this user has NO tasks owned then we can delete him forever :)
+  if( ! db_result(db_query('SELECT COUNT(*) FROM '.PRE.'tasks WHERE owner='.$row['id'] ), 0, 0 ) ) {
+    $content .= "&nbsp;[<a href=\"users.php?x=$x&amp;action=permdel&amp;userid=".$row['id']."\" onclick=\"return confirm( '".sprintf($lang['permdel_javascript_sprt'], javascript_escape($row['fullname'] ) )."' )\">".$lang['permdel']." </a>]";
+  }
+  $content.="</span></td></tr>\n";
+}
+
+$content .= "</table>";
+
+//show it
+new_box($lang['deleted_users'], $content, 'boxdata' );
+
+?>
---
+++ webcollab/users/user_existing_list.php
@@ -0,0 +1,84 @@
+<?php
+/*
+  $Id$
+
+  (c) 2002 - 2005 Andrew Simpson <andrew.simpson at paradise.net.nz>
+  
+  WebCollab
+  ---------------------------------------
+  Based on CoreAPM 2001/2002 by Dennis Fleurbaaij
+  
+  This program is free software; you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software Foundation;
+  either version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with this
+  program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+
+  Function:
+  ---------
+
+  The list box that contains the user-functions
+
+*/
+
+require_once('path.php' );
+require_once(BASE.'includes/security.php' );
+
+$content    = '';
+$allowed[0] = 0;
+
+
+//get list of common users in private usergroups that this user can view 
+$q = db_query('SELECT '.PRE.'usergroups_users.usergroupid AS usergroupid,
+                      '.PRE.'usergroups_users.userid AS userid 
+                      FROM '.PRE.'usergroups_users 
+                      LEFT JOIN '.PRE.'usergroups ON ('.PRE.'usergroups.id='.PRE.'usergroups_users.usergroupid)
+                      WHERE '.PRE.'usergroups.private=1');
+
+for( $i=0 ; $row = @db_fetch_num($q, $i ) ; ++$i ) {
+  if(in_array($row[0], (array)$GID ) && ! in_array($row[1], (array)$allowed ) ) {
+   $allowed[] = $row[1];
+  }
+}
+
+//query
+$q = db_query('SELECT * FROM '.PRE.'users WHERE deleted=\'f\' ORDER by fullname' );
+
+//check for enough users
+if(db_numrows($q) < 1 ) {
+  $content = "<small>".$lang['no_users']."</small>";
+  new_box($lang['users'], $content );
+  return;
+}
+
+$content = "<table class>\n";
+
+//show them
+for($i=0 ; $row = @db_fetch_array($q, $i ) ; ++$i ) {
+
+  //user test for privacy
+  if($row['private'] && ($row['id'] != UID ) && ( ! ADMIN ) && ( ! in_array($row['id'], (array)$allowed ) ) ){
+    continue;
+  }
+
+  $content .= "<tr><td style=\"padding-right: 5px\"><a href=\"users.php?x=$x&amp;action=show&amp;userid=".$row['id']."\">".$row['fullname']."</a></td>";
+
+  if(ADMIN ) {
+    $content .= "<td><span class=\"textlink\"> [<a href=\"users.php?x=$x&amp;userid=".$row['id']."&amp;action=del\">".$lang['del']."</a>]&nbsp;".
+                "[<a href=\"users.php?x=$x&amp;userid=".$row['id']."&amp;action=edit\">".$lang['edit']."</a>]</span></td>";
+  }
+  $content .= "</tr>\n";
+}
+
+$content .= "</table>";
+
+//show it
+new_box($lang['existing_users'], $content, 'boxdata' );
+
+?>
