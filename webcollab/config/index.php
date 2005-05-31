e2e8a443197d3f3a194c136dd8788d85e5a5cc46
andrewsimpson <andrew.simpson@paradise.net.nz>
2005-05-31 05:31:18 +0000

    Initial upload

---
+++ webcollab/config/index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+This file is to prevent users browsing the listing of this directory.
+*/ 
+?>
+
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<HTML><HEAD>
+<TITLE>403 Forbidden</TITLE>
+</HEAD><BODY>
+<H1>Forbidden</H1>
+You don't have permission to access <?php echo $_SERVER['REQUEST_URI']; ?> on this server.<p>
+<HR>
+<?php echo $_SERVER['SERVER_SIGNATURE']; ?>
+</BODY></HTML>
