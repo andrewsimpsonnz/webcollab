65cb5b5f1636319be9ba54a1ee1a51a8b77878e1
andrewsimpson <andrew.simpson@paradise.net.nz>
2005-06-18 20:42:48 +0000

    Syntax error corrected

--- webcollab/config/index.php
+++ webcollab/config/index.php
@@ -9,7 +9,7 @@ This file is to prevent users browsing the listing of this directory.
 <TITLE>403 Forbidden</TITLE>
 </HEAD><BODY>
 <H1>Forbidden</H1>
-You don't have permission to access <?php echo $_SERVER['REQUEST_URI']; ?> on this server.<p>
+You don't have permission to access <?php echo $_SERVER['REQUEST_URI']; ?> on this server.<P>
 <HR>
 <?php echo $_SERVER['SERVER_SIGNATURE']; ?>
 </BODY></HTML>
