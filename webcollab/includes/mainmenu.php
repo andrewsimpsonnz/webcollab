5efe1dfb6ff78ea9f856aecc5354c99deda8ab4a
andrewsimpson <andrew.simpson@paradise.net.nz>
2005-08-24 05:48:01 +0000

    Change calendar action

--- webcollab/includes/mainmenu.php
+++ webcollab/includes/mainmenu.php
@@ -39,7 +39,7 @@ $content = '';
 $content .= "<a href=\"main.php?x=".$x."\">".$lang['home_page']."</a><br />\n".
             "<a href=\"tasks.php?x=".$x."&amp;action=summary\">".$lang['summary_page']."</a><br />\n".
             "<a href=\"tasks.php?x=".$x."&amp;action=todo\">".$lang['todo_list']."</a><br />\n".
-            "<a href=\"calendar.php?x=".$x."\">".$lang['calendar']."</a><br />\n".
+            "<a href=\"calendar.php?x=".$x."&amp;action=show\">".$lang['calendar']."</a><br />\n".
             "<a href=\"archive.php?x=".$x."&amp;action=list\">".$lang['archive']."</a><br />\n".
             "<a href=\"logout.php?x=".$x."\">".$lang['log_out']."</a><br />\n";
 
