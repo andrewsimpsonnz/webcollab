05f5e1df265d9e44056ce17f461c56d368e557ca
andrewsimpson <andrew.simpson@paradise.net.nz>
2005-08-24 05:54:48 +0000

    Rearrange to show edit link for projects where group can edit

--- webcollab/tasks/task_menubox.php
+++ webcollab/tasks/task_menubox.php
@@ -51,31 +51,44 @@ if(! empty($_GET['taskid']) && is_numeric($_GET['taskid']) ){
   require_once(BASE.'includes/details.php' );
   
   //don't show options for archived projects
-  if($TASKID_ROW['archive'] == 0 ){
+  if($TASKID_ROW['archive'] == 0 ) {
     
     $menu_type = $TYPE;
     
-    if((ADMIN ) || ($TASKID_ROW['owner'] == UID )  ) {
-      $content .= "<small><b>".$lang['admin'].":</b></small><br />\n".
-                  "<a href=\"tasks.php?x=".$x."&amp;action=edit&amp;taskid=".$taskid."\">".$lang["edit_".$TYPE]."</a><br />\n".
-                  "<a href=\"tasks.php?x=".$x."&amp;action=delete&amp;taskid=".$taskid."\"  onclick=\"return confirm( '".sprintf($lang["del_javascript_".$TYPE."_sprt"], javascript_escape($TASKID_ROW['name'] ) )."')\">".$lang["delete_$TYPE"]."</a><br />\n".
-                  "<br /><small><b>".$lang['global'].":</b></small><br />\n";
+    //edit, delete rights
+    if((ADMIN ) || 
+       ($TASKID_ROW['owner'] == UID ) || 
+       (($TASKID_ROW['groupaccess'] == "t") && (in_array($TASKID_ROW['usergroupid'], (array)$GID ) ) ) ) {
       
-      if(($TYPE == 'project' ) && ($TASKID_ROW['archive'] == 0 ) ){
-        $archive = "<a href=\"archive.php?x=".$x."&amp;action=submit_archive&amp;taskid=".$taskid."\"  onclick=\"return confirm( '".sprintf($lang['javascript_archive_project'], javascript_escape($TASKID_ROW['name'] ) )."')\">".$lang['archive_project']."</a><br />\n";
+      //header + edit
+      $content .= "<small><b>".$lang['admin'].":</b></small><br />\n".
+                  "<a href=\"tasks.php?x=".$x."&amp;action=edit&amp;taskid=".$taskid."\">".$lang["edit_".$TYPE]."</a><br />\n";
+                  
+      //delete
+      if((ADMIN ) || ($TASKID_ROW['owner'] == UID ) ) { 
+        $content .= "<a href=\"tasks.php?x=".$x."&amp;action=delete&amp;taskid=".$taskid."\"  onclick=\"return confirm( '".sprintf($lang["del_javascript_".$TYPE."_sprt"], javascript_escape($TASKID_ROW['name'] ) )."')\">".$lang["delete_$TYPE"]."</a><br />\n";
       }
+      
+      //clone
+      if(ADMIN ) {
+        $content .= "<a href=\"tasks.php?x=".$x."&amp;action=clone&amp;taskid=".$taskid."\">".$lang["clone_".$TYPE]."</a><br />\n";
+      }
+      
+      //archive project
+      if((ADMIN ) || ($TASKID_ROW['owner'] == UID ) ) {
+        if(($TYPE == 'project' ) && ($TASKID_ROW['archive'] == 0 ) ) {
+          $content .= "<a href=\"archive.php?x=".$x."&amp;action=submit_archive&amp;taskid=".$taskid."\"  onclick=\"return confirm( '".sprintf($lang['javascript_archive_project'], javascript_escape($TASKID_ROW['name'] ) )."')\">".$lang['archive_project']."</a><br />\n";
+        }
+      }          
+      //global header
+      $content .= "<br /><small><b>".$lang['global'].":</b></small><br />\n";
     }
+    //add task
     $content .= "<a href=\"tasks.php?x=".$x."&amp;action=add&amp;parentid=".$taskid."\">".$lang['add_task']."</a><br />\n";
-  
-    if(ADMIN ){
-      $clone = "<a href=\"tasks.php?x=".$x."&amp;action=clone&amp;taskid=".$taskid."\">".$lang["clone_".$TYPE]."</a><br />\n";
-    }
   }
 }
-
-//the task-independent part
+//add project
 $content .= "<a href=\"tasks.php?x=".$x."&amp;action=add\">".$lang['add_project']."</a><br />\n";
-$content .= $archive.$clone;
 
 new_box( $lang[$menu_type."_options"], $content, 'boxmenu' );
 
