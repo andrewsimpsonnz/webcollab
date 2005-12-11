f8d34228e33989a3455a727e539cb9d137b375d0
andrewsimpson <andrew.simpson@paradise.net.nz>
2005-12-11 08:12:31 +0000

    Initial upload

---
+++ webcollab-utf8/lang/lang_list.php
@@ -0,0 +1,54 @@
+<?php
+/*
+  $Id$
+
+  WebCollab
+  ---------------------------------------
+  This file created 2003 by Andrew Simpson.
+
+  This program is free software; you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software Foundation;
+  either version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with this
+  program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+
+  Function:
+  ---------
+
+  Creates a list of languages.
+
+  Note: This file is UTF-8 character set
+   
+*/
+
+
+$lang_list = array();
+
+$lang_list['bg']    = 'Български';
+$lang_list['ca']    = 'Català';
+$lang_list['cs']    = 'Čeština';
+$lang_list['da']    = 'Dansk';
+$lang_list['de']    = 'Deutsch';
+$lang_list['en']    = 'English';
+$lang_list['es']    = 'Español';
+$lang_list['fr']    = 'Français';
+$lang_list['gr']    = 'Ελληνικά';
+$lang_list['hu']    = 'Magyar';
+$lang_list['it']    = 'Italiano';
+$lang_list['ja']    = '日本語';
+$lang_list['ko']    = '한국어';
+$lang_list['pt-br'] = 'Português (Br)';
+$lang_list['ru']    = 'русский';
+$lang_list['se']    = 'Svenska';
+$lang_list['sr']    = 'Srpski';
+$lang_list['tr']    = 'Türkçe';
+$lang_list['zh-cn'] = 'Chinese (Traditional)';
+$lang_list['zh-hk'] = 'Chinese (Simplified)';
+    
+?>
