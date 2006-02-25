6703563f17a04fa94a65a22fbc2b7a1d676dfd14
andrewsimpson <andrew.simpson@paradise.net.nz>
2006-02-25 01:19:54 +0000

    Initial upload

---
+++ webcollab-utf8/lang/sk_long_message.php
@@ -0,0 +1,55 @@
+<?php
+/*
+  $Id$
+
+  WebCollab
+  ---------------------------------------
+  This file created 2005 by Stanislav Pekarčík;fredis@SoftHome.net
+  
+  This program is free software; you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software Foundation;
+  either version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with this
+  program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+
+  Function:
+  ---------
+
+  Language files (long messages) for 'sk' (Slovenský)
+
+  Maintainer: 
+
+  NOTE: This file is written in UTF-8 character set
+
+*/
+
+$taskgroup_info =   "<ul><li>Ak bude zrušená skupina pre úlohu, všetky jej úlohy budú nastavené ako nezaradené.</li>\n".
+                      "<li>Môžte zmeniť meno kategórie bez závislosti s úlohou.</li>\n".
+                      "<li>Dve skupiny pre úlohu nemôžu mať rovnaké meno.</li></ul>\n";
+
+$usergroup_info =   "<ul><li>Ak zrušíte uživateľskú skupinu všetky správy na jej privátnom fóre budú vymazané tiež.</li>\n".
+                      "<li>Privátne uživateľské skupiny sú viditeľné len členom tejto privátnej uživateľskej skupiny.</li>\n".
+                      "<li>Meno uživateľskej skupiny môžte zmeniť bez zmeny o uživateľoch, ktorý sú v nej.</li>\n".
+                      "<li>Dve rôzné uživateľské skupiny nemôžu mať rovnaké mená.</li></ul>\n";
+
+$user_info      =    "Prosím zvoľte Vašu akciu z menu vľavo.<br /><br />".
+                      "Niektoré tipy:<br />".
+                      "<ul>".
+                      "<li>Privátny uživatelia môžu vidieť len členov, korý sú v tej istej skupine.</li>\n".
+                      "<li>Sú dve štádia zrušenia uživateľa, druhé je trvalé.</li>\n".
+                      "<li>Zrušený užívateľ stráca všetky svoje úlohy, ale nie svoje správy na fóre.</li>\n".
+                      "<li>Trvalo zrušený užívateľ stráca všetko.</li>\n".
+                      "<li>Zrušenému užívateľovi ostáva zoznam úloh, a bude pokračovať s týmto zoznamom po obnove.</li>\n".
+                      "<li>Informácie o všetkých akciách vykonaných užívateľom budú uživateľovi poslané emailom.</li>\n".
+                      "<li>Heslo je kryptované v databáze. Vy môžte len nastaviť nové.</li>\n".
+                      "<li>Heslá sú zaslané len pri zadaní, takže budťe opatrný komu ich zasielate emailom!</li>\n".
+                      "<li>Užívateľ môže čiastočne editovať sám seba bez vedomia adminstrátora - to môže ušetriť Váš čas (a spam)</li>\n".
+                      "</ul>\n";
+
+?>
