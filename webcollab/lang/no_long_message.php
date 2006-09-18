2534dab2b3dd89c6b49e19fc4bc988831837fd48
andrewsimpson <andrew.simpson@paradise.net.nz>
2006-09-18 07:16:19 +0000

    Initial upload

---
+++ webcollab/lang/no_long_message.php
@@ -0,0 +1,67 @@
+<?php
+/*
+  $Id$
+
+  WebCollab
+  ---------------------------------------
+  This file created 2003 by Andrew Simpson
+  
+  This program is free software; you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software Foundation;
+  either version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with this
+  program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+
+
+  Function:
+  ---------
+
+  Language files (long messages) for 'no' (Norwegian)
+
+  Maintainer:
+  
+  
+  Translation by Kenneth Aar
+    
+  NOTE: This file is written in ISO-8859-1 character set
+    
+*/
+
+
+$taskgroup_info =   "<ul><li>Hvis du sletter en oppgavegruppe, vil alle oppgaver i den bli endret til ikke-kategoriseret</li>\n".
+                      "<li>Du kan endre navn på en kategori uten å påvirke på oppgaver</li>\n".
+                      "<li>To oppgavegrupper kan ikke ha samme navn</li></ul>\n";
+
+$usergroup_info =   "<ul><li>Hvis du sletter en brukergruppe vil alle tilhørende private forum-poster bli slettet</li>\n".
+                      "<li>Du kan endre navnet på en brukergruppe uten på påvirke på brukerene i den.</li>\n".
+                      "<li>To brukergrupper kan ikke ha samme navn.</li></ul>\n";
+
+$user_info      =    "Velg handling i menyen til venstre.<br /><br />".
+                      "Tips:<br />".
+                      "<ul>".
+                      "<li> Private brukere kan kun sees av medlemmer av den samme brukergruppen.</li>\n".
+                      "<li>Brukere har to trinn ved sletting, det andre trinnet er permanent.</li>\n".
+                      "<li>En slettet bruker mister alle sine oppgaver, men ikke innlegg i forumet.</li>\n".
+                      "<li>En permanent sletter bruker mister alt.</li>\n".
+                      "<li>Du kan ikke slette en bruker permanent, som fremdeles har emner i forumet.</li>\n".
+                      "<li>En slettet bruker beholder sitt oppgavearkiv, og vil fortsette med samme arkiv ved gjennoppretting.</li>\n".
+                      "<li>Alle handlinger utført på en bruker vil bli varslet pr epost til brukeren.</li>\n".
+                      "<li>Passord blir kodet i databasen. Du kan kun velge et nytt.</li>\n".
+                      "<li>Passord blir kun sendt pr epost en gang til brukeren, når de velges, så vær forsiktig med, hvor de sendes!</li>\n".
+                      "<li>Brukere kan selv endre info uten administrators viten - Det sparer deg tid og unøding epost.</li>\n".
+                      "</ul>\n";
+
+$calendar_key    =  "<i>Tilbake til hovedmenyen</i></a>]</b><br />\n".
+                      "<p><b><span class=\"underline\">Forklaring til Kalenderoppføringer</span></b><br /><br />\n".
+                      "<span class=\"blue\">Prosjekter (med uavsluttede oppgaver).</span><br />\n".
+                      "<span class=\"green\"><span class=\"underline\">Prosjekt </span>(alle oppgaver avsluttet.)</span><br />\n".
+                      "<span class=\"red\">Oppgave (ikke avsluttet)</span><br />\n".
+                      "<span class=\"green\">Oppgave (avsluttet)</span><br /></p>\n";
+
+?>
