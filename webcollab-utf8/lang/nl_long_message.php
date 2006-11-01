c4e6ea3c338af28d83d73d0df8f8755f92f0e5ff
andrewsimpson <andrew.simpson@paradise.net.nz>
2006-11-01 07:38:54 +0000

    Initial upload

---
+++ webcollab-utf8/lang/nl_long_message.php
@@ -0,0 +1,56 @@
+<?php
+/*
+  $Id$
+
+  WebCollab
+  ---------------------------------------
+  This file created 2003 by Andrew Simpson
+  
+  This program is free software; you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software Foundation;
+  either version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with this
+  program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+
+
+  Function:
+  ---------
+
+  Language files (long messages) for 'nl' (Nederlands)
+
+  Maintainer: Patrick Flinkerbusch <patrick at flinkerbusch.info>
+
+*/
+
+
+$taskgroup_info =   "<ul><li>Als u een taakgroep verwijderd, worden alle bijbehorende taken aangemerkt als zonder categorie.</li>\n".
+                      "<li>U kunt de naam van een categorie wijzigen zonder de taken te beïnvloeden.</li>\n".
+                      "<li>Twee taakgroepen kunnen niet dezelfde naam hebben.</li></ul>\n";
+
+$usergroup_info =   "<ul><li>Als u een gebruikersgroep verwijderd, worden tevens alle bijbehorende prive forum berichten verwijderd.</li>\n".
+                      "<li>Prive gebruikersgroepen zijn alleen zichtbaar voor de leden van die prive gebruikersgroep.</li>\n".
+                      "<li>U kunt de naam van een gebruikersgroep wijzigen zonder de leden te beïnvloeden.</li>\n".
+                      "<li>Twee gebruikersgroepen kunnen niet dezelfde naam hebben.</li></ul>\n";
+
+$user_info      =    "Maak uw keuze in het menu links.<br /><br />".
+                      "Enkele tips:<br />".
+                      "<ul>".
+                      "<li>Prive gebruikers zijn alleen zichtbaar voor de leden van dezelfde gebruikersgroep.</li>\n".
+                      "<li>Gebruikers kunnen in twee fases verwijderd worden. De tweede fase is onomkeerbaar.</li>\n".
+                      "<li>Een gedeactiveerde gebruiker verliest alle taken maar niet de forum berichten.</li>\n".
+                      "<li>Een verwijderde gebruiker verliest alles.</li>\n".
+                      "<li>Een gedeactiveerde gebruiker houdt een lijst van de taken en kan daarmee doorgaan totdat hij/zij opnieuw geactiveerd wordt.</li>\n".
+                      "<li>ALLE wijzigingen aan een gebruiker worden via email gestuurd naar die gebruiker.</li>\n".
+                      "<li>Wachtwoorden worden versleuteld in de database. U kunt alleen een nieuwe instellen.</li>\n".
+                      "<li>Wachtwoorden worden slechts eenmaal via email gestuurd naar de gebruiker bij het instellen, dus wees voorzichtig als u ze verstuurd!</li>\n".
+                      "<li>Gebruikers kunnen hun gegevens wijzigen zonder dat de beheerder dat merkt - dit bespaart u tijd (en spam)</li>\n".
+                      "</ul>\n";
+
+?>
+
