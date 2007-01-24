1cd5d84af8e80fe77cdbec1d53805c90814d868e
andrewsimpson <andrew.simpson@paradise.net.nz>
2007-01-24 08:17:39 +0000

    Initial upload

---
+++ webcollab-utf8/lang/eo_long_message.php
@@ -0,0 +1,57 @@
+<?php
+/*
+  $Id:  $
+
+  WebCollab
+  ---------------------------------------
+  This file created 2003 by Andrew Simpson
+  
+  This program is free software; you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software Foundation;
+  either version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with this
+  program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+
+
+  Function:
+  ---------
+
+  Language files (long messages) for 'eo' (Esperanto)
+
+  Translator: Paul Ebermann < epaul (at) sourceforge . net >
+
+  NOTE: This file is written in UTF-8 character set
+
+*/
+
+
+$taskgroup_info =   "<ul><li>Se vi forigas taskogrupon, iliaj taskoj iĝos sengrupaj.</li>\n".
+                      "<li>La nomo de taskogrupo estas ŝanĝebla sen influi la arojn de taskoj en ĝi.</li>\n".
+                      "<li>Ne povas ekzisti du taskogrupojn kun sama nomo.</li></ul>\n";
+
+$usergroup_info =   "<ul><li>Se vi forigas uzantogrupon, ĉiuj al ĝi apartenantaj privataj forumaj mesaĝoj ankaŭ malaperos.</li>\n".
+                      "<li>Privataj uzantgrupoj nur estas videbla de membroj de tiuj grupoj (kaj administrantoj).</li>\n".
+                      "<li>La nomo de uzantgrupo estas ŝanĝebla sen influi la aron de uzantoj en ĝi.</li>\n".
+                      "<li>Ne povas ekzisti du taskogrupoj kun sama nomo.</li></ul>\n";
+
+$user_info      =    "Bonvolu elekti agon el la menuo maldekstre.<br /><br />".
+                      "Rimarkoj:<br />".
+                      "<ul>".
+                      "<li>Privataj uzantoj nur povas esti vidata de membroj de la sama uzantogrupo.</li>\n".
+                      "<li>Uzantoj havas du statoj de <em>forigiteco</em> - la dua estas daŭra (nerenversigebla).</li>\n".
+                      "<li>Forigita uzanto perdas siajn Taskojn, sed ne la forumajn mesaĝojn.</li>\n".
+                      "<li>Daŭre forigita uzanto perdis ĉion.</li>\n".
+                      "<li>Forigita uzanto tenas la vid-rajtojn de videblaj taskoj, kaj rericevas ilin post revigigo.</li>\n".
+                      "<li><em>Ĉiuj</em> Agoj, kiujn vi faras pri uzanto, estos senditaj al li per retpoŝto.</li>\n".
+                      "<li>La pasvortoj estas kriptitaj en la datumbazo, do ne eblas eltrovi ilin. Vi nur povas disdoni novajn.</li>\n".
+                      "<li>La pasvortoj estos post ŝanĝo sendataj (unufoje) al la uzanto - do atentu pri la retpoŝtadreso.</li>\n".
+                      "<li>Uzantoj povas redakti sin mem, sen ke administrantoj ekscias pri tio. Tio ŝparos tempon (kaj tro da retmesaĝoj).</li>\n".
+                      "</ul>\n";
+
+?>
