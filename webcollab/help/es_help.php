33fa3d72f5ce21d0d8e682373977bb8dc881e5c0
andrewsimpson <andrew.simpson@paradise.net.nz>
2007-07-25 08:59:39 +0000

    Updating by translator.

--- webcollab/help/es_help.php
+++ webcollab/help/es_help.php
@@ -33,7 +33,7 @@ require_once("path.php" );
 require_once(BASE.'path_config.php' );
 require_once(BASE_CONFIG.'config.php' );
 
-define('CHARACTER_SET', 'ISO-8859-1' );
+define('CHARACTER_SET', 'UTF-8' );
 
 include_once( BASE."includes/screen.php" );
 
@@ -41,42 +41,41 @@ create_top("Help", 1 );
 
 $content = "
 <a name=\"usergroup\"></a>
-<p><b>User groups:</b></p>
-<p>La mayoria de los proyectos y tareas tiene un grupo de usuarios trabajando juntos en una especifica area. Un usergroup es un grupo de usuarios que comparten un area de trabajo similar. Las notificaciones por e-mail pueden ser enviadas al usergroup, mas que a un unico usuario.
+<p><b>Grupos de usuarios:</b></p>
+<p>Los proyectos y tareas pueden asignarse a un grupo de usuarios. Un grupo de usuarios es un conjunto de personas que comparten un área de trabajo similar. Las notificaciones por correo electrónico relativas a ese área de trabajo pueden enviarse al grupo, mas que a un único usuario.
 </p>
-<p>Usergroups puede tambien ser usado para control de acceso. El acceso puede ser limitado a solo un Usergroup; en cuyo caso otros usuarios no podran ver el proyecto/tarea, no estaran habilitados para acceder. La restriccion de acceso puede ser aplicada al proyecto o tarea usando el checkbox 'Todos los usuarios pueden ver esta tarea?'.
+<p>Los grupos de usuarios pueden utilizarse para controlar el acceso. El acceso puede ser limitarse a un grupo concreto; en cuyo caso otros usuarios no podrán ver el proyecto/tarea, ya que no tendrán permisos para acceder. La restricción de acceso puede ser aplicada al proyecto o tarea usando la opción 'Todos los usuarios pueden ver esta tarea?'.
 </p>
-<p>Donde es aplicable, Usergroup pueden tener su propio foro privado para cada tarea o proyecto.
+<p>Un grupo de usuarios puede tener su propio foro privado para cada tarea o proyecto.
 </p>
 <a name=\"taskgroup\"></a>
-<p><b>Task groups:</b></p>
-<p>La diferencia entre Taskgroups y Usergroups no es facil entender a los nuevos usuarios. De todas maneras la diferencia esta bien marcada; Usergroups controla el acceso y flujo de informacion; Taskgroups son meramente para hacer el listado de tareas mas legible.
+<p><b>Grupo de tareas:</b></p>
+<p>La diferencia entre grupo de tareas y grupo de usuarios no es fácil entender a los nuevos usuarios. De todas maneras la diferencia esta bien marcada: grupo de usuarios controla el acceso y flujo de información; los grupos de tareas son meramente para hacer el listado de tareas mas legible.
 </p>
-<p>Cuando un proyecto crece y pasa a tener un numero considerable de tareas, el listado puede aparecer demasiado largo y no muy legible. Poniendo las tareas en Taskgroups la lista sera agrupada en sub.secciones (por Taskgroups) y por lo tanto mas legible. Tareas que no tienen Taskgroups asignado seran agrupadas como Uncategorisied.
+<p>Cuando un proyecto crece y pasa a tener un numero considerable de tareas, el listado puede ser demasiado largo y no muy legible. Poniendo las tareas en grupos la lista sera divide en secciones (por grupos de tareas). Tareas que no tienen grupo asignado aparecen como 'Sin categoría­'.
 </p>
-<p>Para resumir: Si en un proyecto no hay tareas agrupadas en un Taskgroup, el listado de tareas sera una larga lista. Si al menos una de las tareas tiene un Taskgroup, todas las tareas seran listadas por Taskgroups.
+<p>Para resumir: Si en un proyecto no hay tareas agrupadas en un grupos, el listado de tareas sera una larga lista. Si al menos una de las tareas tiene un grupo, todas las tareas serán listadas por grupos de tareas.
 </p>
 <a name=\"globalaccess\"></a>
-<p><b>Todos los usuarios pueden ver la tarea?</b></p>
-<p>Este checkbox permite ver la tarea / proyecto solo a los miembros del grupo del Usergroup seleccionado. Cuando el checkbox es checked, todos los usuarios pueden ver la tarea / proyecto. De lo contrario, cuand checked solo los miembro del USergroup pueden ver la tarea / proyecto.
+<p><b>Abierto a todos los usuarios?</b></p>
+<p>Esta opción permite restringir el acceso a tareas / proyectos. Si la opción esta activada, todos los usuarios pueden ver la tarea / proyecto. De lo contrario, cuando no esta seleccionada solo los miembro del grupo de usuarios pueden ver la tarea / proyecto.
 </p>
-<p>Para Tareas: Ususario que no pertenecen a los Usergroups seleccionados estan habilitados a ver las tareas del proyecto, pero no les sera permitido acceder. 
+<p>Para Tareas: Usuarios que no pertenecen a los grupos seleccionados pueden ver las tareas del proyecto, pero no les sera permitido acceder. 
 </p>
-<p>Para proyectos: Usuarios que no pertenecen a los Usergroups seleccionados no estan habilitados a ver el proyecto o las tareas asociadas en cualquier patanlla
+<p>Para proyectos: Usuarios que no pertenecen a los grupos seleccionados no pueden ver el proyecto o las tareas asociadas.
 </p>
-<p>Si no hay Usergroup seleccionado, este checkbox no tiene efecto.
+<p>Si la tarea o el proyecto no esta asignado a un grupo de usuarios seleccionado, esta opción no tiene efecto.
 </p>
 <a name=\"groupaccess\"></a>
-<p><b>Anyone in the usergroup can edit?</b></p>
-<p>This checkbox allows all members of a Usergroup to edit the task / project.  When the box is
-checked, all members of the Usergroup can edit this item.  If the box is unchecked, only the owner
-can edit the task.
+<p><b>Cualquier miembro del grupo puede editar?</b></p>
+<p>Esta opción permite a todos los miembros del grupo de usuarios a editar la tarea o el proyecto.
+Si la opción esta activada todos tienen permiso para editar. Si esta desactivada solo el propietario de la tarea o el proyecto puede editarlo.
 </p>
-<p>If no Usergroup is selected, this checkbox has no affect.
+<p>Si la tarea o el proyecto no esta asignado a un grupo de usuarios seleccionado, esta opción no tiene efecto.
 </p>
 <a name=\"summarypage\"></a>
-<p><b>Resumen page:</b>
-<p>LA pagina resumen contiene seis columnas que concisamente indican que esta sucediendo con cada tarea.
+<p><b>Pagina resumen:</b>
+<p>La pagina resumen contiene seis columnas que indican brevemente el estado de cada tarea.
 </p>
 <ul>
 <li><b>Banderas</b>:<br />
@@ -84,51 +83,50 @@ indican si hay algo nuevo para cada tarea.
 Las posibilidades son:
 <ul>
 <li><b>C</b>:<br />
-indica que la tarea fue creada</li>
+la tarea acaba de crearse</li>
 <li><b>M</b>:<br />
-indica que la tarea fue modificada</li>
+la tarea ha sido modificada</li>
 <li><b>P</b>:<br />
-(posting) indica que una nota fue hecha en el foro de la tarea</li>
+(posting) indica que hay un nuevo mensaje en el foro de la tarea</li>
 <li><b>F</b>:<br />
-(file) indica que un archivo fue enviado para la tarea</li>
+(file) indica que se ha asociado un archivo nuevo a la tarea</li>
 </ul>
-Si una bandera esta presente,
-entonces puede haceer click en ella para mostrar la tarea asociada.</li>
-<li><b>Deadline</b>:<br />
-indica cuando la tarea esta vencida.
+Al pinchar con el ratón sobre una bandera se muestra la tarea asociada.</li>
+<li><b>Fecha tope</b>:<br />
+indica el plazo en el que la tarea debería haberse realizado.
 Si la fecha aparece en <span class=\"red\">rojo</span>,
-entonces la tarea ya ha vencido;
+entonces el plazo de ejecución ha vencido;
 de otro modo,
 si la fecha aparece en <span class=\"green\">verde</span>,
 entonces la fecha de vencimiento es hoy</li>
 <li><b>Estado</b>:<br />
-inidca el estado del trabajo del item:
+Inidica el estado del trabajo de un proyecto o tarea:
 <ul>
 <li><b>Planeado</b>:<br />
-indica que la tareas fue recientemente creada,
-pero (intencionalmente) no ha sido todavia programada</li>
+indica que la tarea fue creada recientemente,
+pero (intencionalmente) no ha sido todavía programada</li>
 <li><b>Nueva</b>:<br />
-indica que la tarea fue recientemente creada,
-y esta esperando recursos para ser comenzada</li>
+indica que la tarea fue creada recientemente,
+y esta esperando recursos para comenzar</li>
 <li><span class=\"blue\">En suspenso</span>:<br />
 indica que los trabajos sobre la tarea han ido detenidos,
-esperando algun evento externo</li>
+esperando algún acontecimiento externo</li>
 <li><span class=\"orange\">Activa</span>:<br />
-indica que la tarea esta en proceso de ejecucion</li>
+indica que la tarea esta en proceso de ejecución</li>
 <li><span class=\"green\">Hecha</span>:<br />
 indica que la tarea ha sido completada</li>
 </ul>
 </li>
 <li><b>Propietario</b>:<br /> 
-indica a quie ha sido asignada la tarea.
-Puede hacer click en el nombrepara poder ver mas informacion acerca de la persona.</li>
-<li><b>Group</b>:<br /> 
-indica el usergroup o el taskgroup asociado a la tarea.
-Si hace click en la cabecera de la columna,
-se cambia la informacion entre estos dos tipos de grupos.</li>
-<li><b>Task</b>:<br /> 
+indica a quien se ha asignado la tarea.
+Puede pinchar con el ratón en el nombre para poder ver mas información sobre la persona.</li>
+<li><b>Grupo</b>:<br /> 
+indica el grupo de usuarios o el grupo de tareas asociado a la tarea.
+Si pincha con el ratón en la cabecera de la columna,
+se cambia la información entre estos dos tipos de grupos.</li>
+<li><b>Tarea</b>:<br /> 
 indica el nombre de la tarea.
-Puede hacer click en el nombre para ver mas informacion.</li>
+Puede pinchar con el ratón en en el nombre para ver mas información.</li>
 </ul>
 ";
 
--- webcollab/help/es_help_admin.php
+++ webcollab/help/es_help_admin.php
@@ -34,7 +34,7 @@ require_once("path.php" );
 require_once(BASE.'path_config.php' );
 require_once(BASE_CONFIG.'config.php' );
 
-define('CHARACTER_SET', 'ISO-8859-1' );
+define('CHARACTER_SET', 'UTF-8' );
 
 include_once(BASE."includes/screen.php" );
 
@@ -42,36 +42,36 @@ create_top("Help", 1 );
 
 $content = "
 	<a name=\"admin\"></a>
-	<p><b>Admin email:</b></p>
-	<p>Esta es la direccion de email del administrador del sitio que se encarga del dia a dia.
+	<p><b>email administracion:</b></p>
+	<p>Esta es la direccion de correo electronico del administrador del sitio que se encarga de la gestion y mantenimiento de la aplicacion.
 	</p>
-	<p>Email automatizados desde este sitio, particularmente lo que concierne a las cuentas de los usuarios, tendran esta direccion como persona de contacto.
+	<p>Los envios automatizados desde este sitio, particularmente lo que concierne a las cuentas de los usuarios, se envian con esta direccion de correo electronico.
 	</p>
-	<p>Esta direccion debe ser siempre ingresada. Si tiene duda, ingrese su direccion de email aqui!
+	<p>Esta direccion debe rellenarse siempre. Si tiene dudas, ponga su propia direccion de correo electronico aqui.
 	</p>
 	<a name=\"reply\"></a>
-	<p><b>Email 'reply to':</b></p>
-	<p>El campo cabecera 'reply-to' para los emails de este sitio.
+	<p><b>Email 'responder a':</b></p>
+	<p>El campo cabecera 'reply-to' para los envios de correo electronico de este sitio.
 	</p>
-	<p>Si tiene duda, inrese el email del administrador.
+	<p>Si tiene dudas, ponga la direccion de correo electronico que puso en email administracion.
 	</p>
 	<a name=\"from\"></a>
-	<p><b>Email 'from':</b></p>
-	<p>El campo cabecera 'from' para los emails salidos desde este sisitio.
+	<p><b>Email 'de'::</b></p>
+	<p>El campo cabecera 'from' para los envios de correo electronico de este sitio.
 	</p>
-	<p>Si tiene duda, ingrese el mail del administrador.
+	<p>Si tiene dudas, ponga la direccion de correo electronico que puso en email administracion.
 	</p>
 	<a name=\"list\"></a>
-	<p><b>Lista de correo (Mailing list):</b></p>
-	<p>Cuando se envia email a los grupos de usuarios (usergroups), tambien seran enviados a la direcciones de email listadas aqui.
-	Esta funcion is para mantenerse iformado del estado del Project Manager.
+	<p><b>Lista de correo:</b></p>
+	<p>Cuando se envian correos electronicos alos grupos de usuarios, tambien seran enviados a la direcciones de email listadas aqui.
+	Esta funcion permite difundir informacion sobre la evolucion de los proyectos.
 	</p>
-	<p>Para que se envien email a los grupos de usuarios (usergroups), el checkbox 'Enviar al Usergroup' en el formulario de agregar o editar Proyecto/Tarea, debe ser chequeado.
-	<p>Esto puede ser hecho por default seteandolo debajo, o en forma manual por el usuario chequeando el checkbox.
+	<p>Para que se envien correos a los grupos de usuarios, la opcion 'Enviar al Usergroup' en el formulario de agregar o editar Proyecto/Tarea, debe estar activada.
+	<p>Esto se puede configurar como opcion predefinida o manualmente para cada proyecto y tarea.
 	</p>
-	<p>Notar que los usuarios pueden cambiar es estado default cambiand el check del checkbox manualmente.
+	<p>Los usuarios pueden cambiar el estado predefinido desactivando la opcion manualmente.
 	</p>
-	<p>Seteando al grupo de usuario (usergroups) como privado no afecta a la lista de correo.
+	<p>Configurar un grupo de usuarios como privado no afecta a la lista de correo.
 	</p>
         ";
 
