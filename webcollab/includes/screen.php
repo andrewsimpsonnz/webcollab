65f6f937f53853810c8e09cc6c0cdf353615473a
andrewsimpson <andrew.simpson@paradise.net.nz>
2007-08-03 22:00:13 +0000

    Bugfix: link reference for favicon in help files is wrong

--- webcollab/includes/screen.php
+++ webcollab/includes/screen.php
@@ -133,7 +133,7 @@ function create_top($title='', $page_type=0, $cursor=0, $check=0, $date=0, $cale
       break;
   }
 
-  $content .= "<link rel=\"icon\" type=\"image/png\" href=\"images/group.png\" />\n";
+  $content .= "<link rel=\"icon\" type=\"image/png\" href=\"".BASE."images/group.png\" />\n";
 
   //javascript scripts
   if($cursor || $check || $date || $calendar ) {
