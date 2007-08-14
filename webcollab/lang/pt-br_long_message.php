a618db04816931c5e77ff45b0b73dd03a7b28429
andrewsimpson <andrew.simpson@paradise.net.nz>
2007-08-14 08:54:10 +0000

    Updating by translator

--- webcollab/lang/pt-br_long_message.php
+++ webcollab/lang/pt-br_long_message.php
@@ -23,7 +23,7 @@
 
   Email text language files for 'pt_br' (Brazilian Portuguese)
 
-  Translation: Eduardo Alvim <eduardoalvim at terra.com.br>
+  Translation: Eduardo Alvim <eduardoalvim at gmail.com>
 
 */
 
@@ -31,15 +31,15 @@ $taskgroup_info =   "<ul><li>Se você excluir um grupo de tarefas, todas as taref
                       "<li>Você pode alterar o nome de uma categoria sem interferir nas tarefas.</li>\n".
                       "<li>Dois grupos de tarefas não podem ter nomes iguais.</li></ul>\n";
 
-$usergroup_info =   "<ul><li>Se você excluir um grupo de usuários, todas as mensagens particulares relacionadas a ele existentes nos fóruns também o serão.</li>\n".
-                      "<li>Grupos privativos de usuários só podem ser enxergados por seus membros.</li>\n".
-                      "<li>Você pode alterar o nome de um grupo de usuários sem interferir em seus membros.</li>\n".
-                      "<li>Dois grupos de usuários não podem ter nomes iguais.</li></ul>\n";
+$usergroup_info =   "<ul><li>Se você excluir uma equipe todas as mensagens particulares relacionadas a ela existentes nos fóruns também o serão.</li>\n".
+                      "<li>Equipes privativas só podem ser enxergadas por seus membros.</li>\n".
+                      "<li>Você pode alterar o nome de uma equipe sem interferir em seus membros.</li>\n".
+                      "<li>Duas equipes não podem ter nomes iguais.</li></ul>\n";
 
 $user_info      =    "Por favor, escolha o que fazer usando o menu à esquerda.<br /><br />".
                       "Algumas dicas rápidas:<br />".
                       "<ul>".
-                      "<li>Usuários privativos só podem ser enxergados por membros do mesmo grupo de usuários.</li>\n".
+                      "<li>Usuários privativos só podem ser enxergados por membros da mesma equipe.</li>\n".
                       "<li>A exclusão de usuários possui dois estágios, sendo o segundo caracterizado por seu caráter permanente.</li>\n".
                       "<li>Um usuário que é excluído perde todas as suas tarefas, mas mantém suas mensagens nos fóruns.</li>\n".
                       "<li>Um usuário que é permanentemente excluído perde tudo.</li>\n".
