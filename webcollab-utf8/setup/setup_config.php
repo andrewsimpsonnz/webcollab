9d1ab76c9e4c70c9ffa6981cfee393d45a159a6d
andrewsimpson <andrew.simpson@paradise.net.nz>
2008-02-21 08:12:00 +0000

    Update

--- webcollab-utf8/setup/setup_config.php
+++ webcollab-utf8/setup/setup_config.php
@@ -3,19 +3,8 @@
   $Id$
 
 */
-   //DEFAULT SETUP FOR UNICODE VERSION 
 
-  //character set
-  define('SETUP_CHARACTER_SET', "UTF-8" );
-
-  //database character set for MySQL
-  define('MYSQL_SETUP_CHARACTER_SET', "utf8" );
-
-  //database character set for PostgreSQL
-  define('PGSQL_SETUP_CHARACTER_SET', "UTF8" );
-
-
-  //Style sheets (CSS) (Place your CSS into /css directory)
+  //Style sheet (CSS) (Place your CSS into /css directory)
   define('SETUP_CSS', 'default.css' );
 
   //version info
