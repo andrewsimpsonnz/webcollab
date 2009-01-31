87bc8b0a91f3acaea0cf9e54994da8b6bd9dceb7
andrewsimpson <andrew.simpson@paradise.net.nz>
2009-01-31 08:36:12 +0000

    Update

--- webcollab/config/index.php
+++ webcollab/config/index.php
@@ -1,15 +1,20 @@
 <?php
-/*
-This file is to prevent users browsing the listing of this directory.
-*/ 
-?>
 
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<HTML><HEAD>
-<TITLE>403 Forbidden</TITLE>
-</HEAD><BODY>
-<H1>Forbidden</H1>
-You don't have permission to access <?php echo $_SERVER['REQUEST_URI']; ?> on this server.<P>
-<HR>
-<?php echo $_SERVER['SERVER_SIGNATURE']; ?>
-</BODY></HTML>
+header("HTTP/1.0 403 Forbidden", true, 403 );
+
+echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"  \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n".
+     "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n".
+     "<head>\n".
+     "<title>403 Forbidden</title>\n".
+     "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n".
+     "</head>\n".
+     "<body>\n".
+     "<h1>Forbidden</h1>\n".
+     "<p>You don't have permission to access ".$_SERVER['REQUEST_URI']." on this server.<\p>\n".
+     "<hr />\n";
+
+echo $_SERVER['SERVER_SIGNATURE']."\n";
+
+echo "</body>\n".
+     "</html>\n";
+?>
