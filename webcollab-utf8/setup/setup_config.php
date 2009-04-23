40f6e0581b6a74e4ea19cf436462c46737910bc4
andrewsimpson <andrew.simpson@paradise.net.nz>
2009-04-23 05:29:56 +0000

    Updating

--- webcollab-utf8/setup/setup_config.php
+++ webcollab-utf8/setup/setup_config.php
@@ -8,7 +8,7 @@
   define('SETUP_CSS', 'setup.css' );
 
   //version info
-  define('SETUP_WEBCOLLAB_VERSION', "2.40" );
+  define('SETUP_WEBCOLLAB_VERSION', "2.50" );
   define('SETUP_UNICODE_VERSION', "Y" );
 
   //available language array
