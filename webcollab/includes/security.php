991239a32c00613e7cda8194a0b84524cf8d30d4
andrewsimpson <andrew.simpson@paradise.net.nz>
2010-04-13 07:14:50 +0000

    Remove preg_match for speed up Update copyrights

--- webcollab/includes/security.php
+++ webcollab/includes/security.php
@@ -2,7 +2,7 @@
 /*
   $Id$
 
-  (c) 2002 - 2009 Andrew Simpson <andrew.simpson at paradise.net.nz>
+  (c) 2002 - 2010 Andrew Simpson <andrew.simpson at paradise.net.nz>
 
   WebCollab
   ---------------------------------------
@@ -49,11 +49,11 @@ if( ! ($ip = $_SERVER['REMOTE_ADDR'] ) ) {
 }
 
 //$session_key can be from either a GET, POST or COOKIE - check for cookie first
-if(isset($_COOKIE['webcollab_session'] ) && preg_match('/^[a-f\d]{32}$/iD', $_COOKIE['webcollab_session'] ) ) {
+if(isset($_COOKIE['webcollab_session'] ) && (strlen(trim($_COOKIE['webcollab_session'], '1234567890abcdefABCDEF' ) ) == 0 ) ) {
   $session_key = db_escape_string($_COOKIE['webcollab_session'] );
   define('X', 0 );
 }
-elseif(isset($_REQUEST['x'] ) && preg_match('/^[a-f\d]{32}$/iD', $_REQUEST['x'] ) ) {
+elseif(isset($_REQUEST['x'] ) && (strlen(trim($_REQUEST['x'], '1234567890abcdefABCDEF' ) ) == 0 ) ) {
   $session_key = db_escape_string($_REQUEST['x']);
   $x = $_REQUEST['x'];
   define('X', $x );
