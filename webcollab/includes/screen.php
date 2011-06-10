52e4c1b1e96f0d1451663f0e14f7a869cafb9aed
andrewsimpson <andrew.simpson@paradise.net.nz>
2011-06-11 08:43:07 +1200

    Update copyrights

--- webcollab/includes/screen.php
+++ webcollab/includes/screen.php
@@ -275,7 +275,7 @@ function create_bottom() {
 
  //shows the logo
  if($bottom_text) {
-   $content .= "<div class=\"bottomtext\" ".$align.">Powered by&nbsp;<a href=\"http://webcollab.sourceforge.net/\" onclick=\"window.open('http://webcollab.sourceforge.net/'); return false\">WebCollab</a>&nbsp;&copy;&nbsp;2002-2010</div>\n";
+   $content .= "<div class=\"bottomtext\" ".$align.">Powered by&nbsp;<a href=\"http://webcollab.sourceforge.net/\" onclick=\"window.open('http://webcollab.sourceforge.net/'); return false\">WebCollab</a>&nbsp;&copy;&nbsp;2002-2011</div>\n";
  }
   //end xml parsing
   $content .= "</body>\n</html>\n";
