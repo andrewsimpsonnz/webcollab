f45544dfdc7ac5bc15dfdbad1127fb3542a69b50
andrewsimpson <andrew.simpson@paradise.net.nz>
2011-09-21 15:28:42 +1200

    Provide output buffering Add zlib.output_compression Formatting

--- webcollab/includes/screen.php
+++ webcollab/includes/screen.php
@@ -66,151 +66,158 @@
 
 function create_top($title='', $page_type=0, $body_id=0, $include_javascript=0, $redirect_time=0 ) {
 
-
-
   global $lang, $bottom_text;
 
-  //start output buffering
+  //we only send the headers and HTML head once...
+  if(! defined('HEADER_DONE' ) ) {
+
+    //we don't want any caching of these pages
+    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+    header('Cache-Control: no-store, no-cache, must-revalidate');
+    header('Cache-Control: post-check=0, pre-check=0', false);
+    header('Pragma: no-cache');
+    header('Content-Type: text/html; charset=UTF-8' );
+
+    //do a refresh if required
+    if($redirect_time != 0) {
+      header('Refresh: '.$redirect_time.'; url='.BASE_URL.'index.php' );
+    }
+
+    if(COMPRESS_OUTPUT == 'Y' ) {
+      ini_set('zlib.output_compression', 'On' );
+    }
+
+    //start output buffering for head
+    ob_start();
+
+    echo     "<!DOCTYPE html PUBLIC\n".
+             "\"-//W3C//DTD XHTML 1.0 Strict//EN\"\n".
+             "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n".
+             "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"".XML_LANG."\" lang=\"".XML_LANG."\">\n\n".
+             "<!-- WebCollab ".WEBCOLLAB_VERSION." -->\n".
+             "<!-- (c) 2001 Dennis Fleurbaaij created for core-lan.nl -->\n".
+             "<!-- (c) 2002-2011 Andrew Simpson for WebCollab -->\n\n".
+             "<head>\n";
+
+    if( $title == '' ) {
+      $title = MANAGER_NAME;
+    }
+
+    echo     "<title>".$title."</title>\n".
+             "<meta http-equiv=\"Pragma\" content=\"no-cache\" />\n".
+             "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=".CHARACTER_SET."\" />\n";
+
+    //do a refresh if required
+    if($redirect_time != 0) {
+      echo   "<meta http-equiv=\"Refresh\" content=\"".$redirect_time.";url=".BASE_URL."index.php\" />\n";
+    }
+
+    switch($page_type) {
+      case 2: //print
+        echo "<link rel=\"StyleSheet\" href=\"".BASE_CSS.CSS_PRINT."\" type=\"text/css\" />\n";
+        break;
+
+      case 3: //calendar
+        echo "<link rel=\"StyleSheet\" href=\"".BASE_CSS.CSS_MAIN."\" type=\"text/css\" />\n".
+             "<link rel=\"StyleSheet\" href=\"".BASE_CSS.CSS_CALENDAR."\" type=\"text/css\" />\n";
+        break;
+
+      case 0: //main window + menu sidebar
+      case 1: //single main window (no menu sidebar)
+      default:
+        echo "<link rel=\"StyleSheet\" href=\"".BASE_CSS.CSS_MAIN."\" type=\"text/css\" />\n";
+        //allow RSS autodiscovery if enabled
+        break;
+    }
+
+    if(defined('RSS_AUTODISCOVERY' ) && RSS_AUTODISCOVERY == 'Y' ) {
+      echo   "<link rel=\"alternate\" href=\"".BASE_URL."rss/rss_projects.php\" type=\"application/rss+xml\" title=\"Projects\" />\n".
+             "<link rel=\"alternate\" href=\"".BASE_URL."rss/rss_tasks.php\" type=\"application/rss+xml\" title=\"Tasks\" />\n".
+             "<link rel=\"alternate\" href=\"".BASE_URL."rss/rss_files.php\" type=\"application/rss+xml\" title=\"Files\" />\n".
+             "<link rel=\"alternate\" href=\"".BASE_URL."rss/rss_forum.php\" type=\"application/rss+xml\" title=\"Forum\" />\n";
+    }
+
+    echo     "<link rel=\"icon\" type=\"image/png\" href=\"".BASE."images/group.png\" />\n";
+
+    switch($include_javascript ) {
+
+      case 1:
+        //loads javascript file
+        echo "<script type=\"text/javascript\" src=\"".BASE_URL."js/webcollab.js\"></script>\n".
+             "<script type=\"text/javascript\" src=\"".BASE_URL."js/bbeditor.js\"></script>\n".
+             "</head>\n\n";
+        break;
+
+      case 2:
+        //loads javascript file (but not editor)
+        echo "<script type=\"text/javascript\" src=\"".BASE_URL."js/webcollab.js\"></script>\n".
+             "</head>\n\n";
+        break;
+
+      case 0:
+      default:
+        //no javascript loaded
+        break;
+    }
+
+    //flush the current buffer and mark headers as done
+    ob_end_flush();
+    define('HEADER_DONE', 1 );
+  }
+  
+  //start new buffer for body of page
   ob_start();
-
-  //we don't want any caching of these pages
-  header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-  header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-  header('Cache-Control: no-store, no-cache, must-revalidate');
-  header('Cache-Control: post-check=0, pre-check=0', false);
-  header('Pragma: no-cache');
-  header('Content-Type: text/html; charset=UTF-8' );
-
-  //do a refresh if required
-  if($redirect_time != 0) {
-    header('Refresh: '.$redirect_time.'; url='.BASE_URL.'index.php' );
-  }
-
-  $content =        "<!DOCTYPE html PUBLIC\n".
-                    "\"-//W3C//DTD XHTML 1.0 Strict//EN\"\n".
-                    "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n".
-                    "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"".XML_LANG."\" lang=\"".XML_LANG."\">\n\n".
-                    "<!-- WebCollab ".WEBCOLLAB_VERSION." -->\n".
-                    "<!-- (c) 2001 Dennis Fleurbaaij created for core-lan.nl -->\n".
-                    "<!-- (c) 2002-2011 Andrew Simpson for WebCollab -->\n\n".
-                    "<head>\n";
-
-  if( $title == '' ) {
-    $title = MANAGER_NAME;
-  }
-
-  $content .=       "<title>".$title."</title>\n".
-                    "<meta http-equiv=\"Pragma\" content=\"no-cache\" />\n".
-                    "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=".CHARACTER_SET."\" />\n";
-
-  //do a refresh if required
-  if($redirect_time != 0) {
-    $content .=     "<meta http-equiv=\"Refresh\" content=\"".$redirect_time.";url=".BASE_URL."index.php\" />\n";
-  }
-
-  switch($page_type) {
-    case 2: //print
-      $content .=   "<link rel=\"StyleSheet\" href=\"".BASE_CSS.CSS_PRINT."\" type=\"text/css\" />\n";
-      break;
-
-    case 3: //calendar
-      $content .=   "<link rel=\"StyleSheet\" href=\"".BASE_CSS.CSS_MAIN."\" type=\"text/css\" />\n".
-                    "<link rel=\"StyleSheet\" href=\"".BASE_CSS.CSS_CALENDAR."\" type=\"text/css\" />\n";
-      break;
-
-    case 0: //main window + menu sidebar
-    case 1: //single main window (no menu sidebar)
-    default:
-      $content .=   "<link rel=\"StyleSheet\" href=\"".BASE_CSS.CSS_MAIN."\" type=\"text/css\" />\n";
-      //allow RSS autodiscovery if enabled
-      break;
-  }
-
-  if(defined('RSS_AUTODISCOVERY' ) && RSS_AUTODISCOVERY == 'Y' ) {
-    $content .=   "<link rel=\"alternate\" href=\"".BASE_URL."rss/rss_projects.php\" type=\"application/rss+xml\" title=\"Projects\" />\n".
-                  "<link rel=\"alternate\" href=\"".BASE_URL."rss/rss_tasks.php\" type=\"application/rss+xml\" title=\"Tasks\" />\n".
-                  "<link rel=\"alternate\" href=\"".BASE_URL."rss/rss_files.php\" type=\"application/rss+xml\" title=\"Files\" />\n".
-                  "<link rel=\"alternate\" href=\"".BASE_URL."rss/rss_forum.php\" type=\"application/rss+xml\" title=\"Forum\" />\n";
-  }
-
-  $content .= "<link rel=\"icon\" type=\"image/png\" href=\"".BASE."images/group.png\" />\n";
-
-  switch($include_javascript ) {
-
-    case 1:
-      //loads javascript file
-      $content .= "<script type=\"text/javascript\" src=\"".BASE_URL."js/webcollab.js\"></script>\n".
-                  "<script type=\"text/javascript\" src=\"".BASE_URL."js/bbeditor.js\"></script>\n";
-       break;
-
-    case 2:
-      //loads javascript file (but not editor)
-      $content .= "<script type=\"text/javascript\" src=\"".BASE_URL."js/webcollab.js\"></script>\n";
-      break;
-
-    case 0:
-    default:
-      //no javascript loaded
-      break;
-  }
 
   switch($body_id ) {
     case true:
-      $content .= "</head>\n\n".
-                  "<body id=\"$body_id\">\n";
-       break;
-
-    case false:
-      $content .= "</head>\n\n".
-                  "<body>\n";
+      echo   "<body id=\"$body_id\">\n";
       break;
 
+    case false:
+      echo   "<body>\n";
+      break;
   }
 
   switch ($page_type ) {
 
     case 0: //main window + menu sidebar
       //create the masthead part of the main window
-      $content .=   "<div id=\"container\">\n".
-                    "<div id=\"top\" class=\"masthead\">";
+      echo   "<div id=\"container\">\n".
+             "<div id=\"top\" class=\"masthead\">";
       //show username if applicable
       if(defined('UID_NAME') ) {
-        $content .= '&nbsp;'.sprintf( $lang['user_homepage_sprt'], UID_NAME );
+        echo '&nbsp;'.sprintf( $lang['user_homepage_sprt'], UID_NAME );
       }
-      $content .=   "</div>\n\n";
+      echo   "</div>\n\n";
       //create menu sidebar
-      $content .=    "<!-- start menu -->\n".
-                     "<div id=\"menu\">\n";
+      echo   "<!-- start menu -->\n".
+             "<div id=\"menu\">\n";
       $bottom_text = 1;
       break;
 
     case 1: //single main window (no menu sidebar)
     case 3: //calendar
-      $content .=   "<div id=\"container\">\n".
-                    "<div id=\"top\" class=\"masthead\">";
+      echo   "<div id=\"container\">\n".
+             "<div id=\"top\" class=\"masthead\">";
       if(defined('UID_NAME' ) ) {
-        $content .= '&nbsp;'.sprintf( $lang['user_homepage_sprt'], UID_NAME );
+        echo '&nbsp;'.sprintf( $lang['user_homepage_sprt'], UID_NAME );
       }
-      $content .=   "</div>\n\n";
+      echo   "</div>\n\n";
       //create single window over entire screen
-      $content .=   "<!-- start single main table -->\n".
-                    "<div id=\"single\">\n";
+      echo   "<!-- start single main table -->\n".
+             "<div id=\"single\">\n";
       $bottom_text = 2;
       break;
 
     case 2: //printable screen
       //create single window over paper width
-      $content .=   "<!-- start single main table -->\n".
-                    "<div id=\"container\">\n".
-                    "<div id=\"single\">\n";
+      echo   "<!-- start single main table -->\n".
+             "<div id=\"container\">\n".
+             "<div id=\"single\">\n";
       //don't want bottom text
       $bottom_text = 0;
   }
-
-  //flush buffer
-  echo $content;
-  ob_end_flush();
-
   return;
 }
 
@@ -256,7 +263,7 @@ function create_bottom() {
   global $bottom_text;
 
   //end the main & container
-  $content = "</div><!-- end main -->\n";
+  echo "</div><!-- end main -->\n";
 
   switch($bottom_text) {
 
@@ -273,12 +280,14 @@ function create_bottom() {
 
  //shows the logo
  if($bottom_text) {
-   $content .= "\n<div id=\"bottom\" class=\"bottomtext\" ".$align.">Powered by&nbsp;<a href=\"http://webcollab.sourceforge.net/\" onclick=\"window.open('http://webcollab.sourceforge.net/'); return false\">WebCollab</a>&nbsp;&copy;&nbsp;2002-2011</div>\n";
+   echo "\n<div id=\"bottom\" class=\"bottomtext\" ".$align.">Powered by&nbsp;<a href=\"http://webcollab.sourceforge.net/\" onclick=\"window.open('http://webcollab.sourceforge.net/'); return false\">WebCollab</a>&nbsp;&copy;&nbsp;2002-2011</div>\n";
  }
   //end xml parsing
-  $content .= "</div><!-- end container -->\n".
-              "</body>\n</html>\n";
-  echo $content;
+  echo "</div><!-- end container -->\n".
+       "</body>\n</html>\n";
+  
+  //flush buffers to web browser
+  ob_end_flush();
   return;
 }
 
