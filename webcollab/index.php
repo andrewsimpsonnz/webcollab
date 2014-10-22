66bfd55421a6ef45fe2e2181bf8d230117ec69e3
Andrew Simpson <andrew.simpson@paradise.net.nz>
2014-10-22 20:40:10 +1300

    Update copyrights

--- webcollab/index.php
+++ webcollab/index.php
@@ -2,7 +2,7 @@
 /*
   $Id: index.php 2288 2009-08-22 08:50:00Z andrewsimpson $
 
-  (c) 2002 - 2013 Andrew Simpson <andrew.simpson at paradise.net.nz>
+  (c) 2002 - 2014 Andrew Simpson <andrew.simpson at paradise.net.nz>
 
   WebCollab
   ---------------------------------------
