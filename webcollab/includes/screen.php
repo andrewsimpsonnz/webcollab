8f0497e66a29dc15989c037214c36c9cc7e58121
Andrew Simpson <andrew.simpson@paradise.net.nz>
2017-08-19 15:29:48 +1200

    Change to HTML5 - allow for XHTML alternate

--- webcollab/includes/screen.php
+++ webcollab/includes/screen.php
@@ -91,10 +91,14 @@ function create_top($title='', $page_type=0, $body_id=0, $include_javascript=0, 
     //start output buffering for head
     ob_start();
 
-    echo     "<!DOCTYPE html PUBLIC\n".
-             "\"-//W3C//DTD XHTML 1.0 Strict//EN\"\n".
-             "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n".
-             "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"".XML_LANG."\" lang=\"".XML_LANG."\">\n\n".
+    echo     # Next four lines are for XHTML page
+             #"<!DOCTYPE html PUBLIC\n".
+             #"\"-//W3C//DTD XHTML 1.0 Strict//EN\"\n".
+             #"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n".
+             #"<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"".XML_LANG."\" lang=\"".XML_LANG."\">\n\n".
+             # Next two lines for HTML5 page
+             "<!DOCTYPE html>\n".
+             "<html lang=\"".XML_LANG."\">\n\n".
              "<!-- WebCollab ".WEBCOLLAB_VERSION." -->\n".
              "<!-- (c) 2001 Dennis Fleurbaaij created for core-lan.nl -->\n".
              "<!-- (c) 2002-2017 Andrew Simpson for WebCollab -->\n\n".
@@ -105,8 +109,11 @@ function create_top($title='', $page_type=0, $body_id=0, $include_javascript=0, 
     }
 
     echo     "<title>".$title."</title>\n".
-             "<meta http-equiv=\"Pragma\" content=\"no-cache\" />\n".
-             "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n";
+             # Next two lines not valid in HTML5 and probably not valid in XHTML
+             #"<meta http-equiv=\"Pragma\" content=\"no-cache\" />\n".
+             #"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n";
+             # Next line for HTML5 and not XHTML
+             "<meta charset=\"utf-8\" />\n";
 
     //do a refresh if required
     if($redirect_time != 0) {
