9f3ca72e0822826d2a486a395d983c6e0735f808
Andrew Simpson <andrew.simpson@paradise.net.nz>
2019-09-05 20:03:00 +1200

    Updated French translations. Thanks to martinbrait

--- webcollab/help/fr_help.php
+++ webcollab/help/fr_help.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /*
   $Id$
 
@@ -39,95 +39,95 @@ create_top("Aide", 1 );
 $content = "
 <a name=\"usergroup\"></a>
 <p><b>Groupe d'utilisateurs:</b></p>
-<p>La plupart des projets ou tâches sont gérés par un groupe d'utilisateurs travaillant ensemble dans un domaine spécifique.  Un groupe d'utilisateurs est
+<p>La plupart des projets ou travaux sont gérés par un groupe d'utilisateurs travaillant ensemble dans un domaine spécifique.  Un groupe d'utilisateurs est
 un groupe d'utilisateurs partageant un même espace de travail. Des courriels de notification peuvent être envoyés à un groupe d'utilisateurs de son choix,
 plutôt qu'à un utilisateur individuel.</p>
-<p>Les groupes d'utilisateurs peuvent également être utilisés pour contrôler l'accessibilité de communications de moindre importance, à travers un forum privé. L'accès peut être limité au seul groupe d'utilisateurs; Dans ce cas, les autres utilisateurs ne verront pas le projet / la tâche ou ne pourront pas y accéder.
-La restriction d'accès peut être appliquée au niveau du projet ou de la tâche à l'aide de la commande \"Tous les utilisateurs peuvent afficher cette
-tâche\"</p>
-<p>Le cas échéant, les groupes d'utilisateurs disposent également de leurs propres forums privés avec chaque tâche et projet.</p>
+<p>Les groupes d'utilisateurs peuvent également être utilisés pour contrôler l'accessibilité de communications de moindre importance, à travers un forum privé. L'accès peut être limité au seul groupe d'utilisateurs; Dans ce cas, les autres utilisateurs ne verront pas le projet / le travail ou ne pourront pas y accéder.
+La restriction d'accès peut être appliquée au niveau du projet ou du travail à l'aide de la commande \"Tous les utilisateurs peuvent afficher cette
+travail\"</p>
+<p>Le cas échéant, les groupes d'utilisateurs disposent également de leurs propres forums privés avec chaque travail et projet.</p>
 <br /> 
 <a name=\"taskgroup\"></a>
-<p><b>Groupe de tâches :</b></p>
-<p>Les différences entre les groupes de tâches et les groupes d'utilisateurs n'apparaissent pas facilement pour les nouveaux utilisateurs.
+<p><b>Groupe de travaux :</b></p>
+<p>Les différences entre les groupes de travaux et les groupes d'utilisateurs n'apparaissent pas facilement pour les nouveaux utilisateurs.
 Cependant la différence est claire; Les groupes d'utilisateurs contrôlent l'accès et le flux d'informations; Les groupes de travail sont
-simplement pour rendre la répartition des tâches plus lisible.</p>
-<p>Lorsqu'un projet comporte un certain nombre de tâches enfants, la liste peut paraître longue et
-illisible.  En disposant les tâches dans un groupe de tâches, la liste sera automatiquement regroupée en
-sous-sections très lisibles(par groupe de travail).  Les tâches auxquelles aucun groupe de tâches n'aura été attribué seront
+simplement pour rendre la répartition des travaux plus lisible.</p>
+<p>Lorsqu'un projet comporte un certain nombre de travaux enfants, la liste peut paraître longue et
+illisible.  En disposant les travaux dans un groupe de travaux, la liste sera automatiquement regroupée en
+sous-sections très lisibles(par groupe de travail).  Les travaux auxquelles aucun groupe de travaux n'aura été attribué seront
 groupées comme 'hors-catégorie'.</p>
-<p>Pour résumer: Si aucune tâche dans un projet n'est classée dans un groupe de tâches, la liste des tâches sera une longue liste illisible.
-Si une tâche au moins est classée dans un groupe de tâches, toutes les tâches seront répertoriées par groupe de tâches.</p>
+<p>Pour résumer: Si aucun travail dans un projet n'est classée dans un groupe de travaux, la liste des travaux sera une longue liste illisible.
+Si un travail au moins est classé dans un groupe de travaux, toutes les travaux seront répertoriées par groupe de travaux.</p>
 <br />
 <a name=\"globalaccess\"></a>
-<p><b>Tous les utilisateurs peuvent-ils voir la tâche ?</b></p>
-<p>Cette case à cocher permet de limiter la visualisation d’une tâche / projet aux membres du groupe sélectionné.
-Lorsque la case est cochée, tous les utilisateurs peuvent voir la tâche / le projet. Inversement, lorsque la case est
-décochée, seuls les membres du groupe d'utilisateurs peuvent voir la tâche.</p>
-<p>Pour les tâches: les utilisateurs n'appartenant pas au groupe d'utilisateurs sélectionné pourront voir les tâches dans la liste de projets,
+<p><b>Tous les utilisateurs peuvent-ils voir le travail ?</b></p>
+<p>Cette case à cocher permet de limiter la visualisation d’un travail / projet aux membres du groupe sélectionné.
+Lorsque la case est cochée, tous les utilisateurs peuvent voir le travail / le projet. Inversement, lorsque la case est
+décochée, seuls les membres du groupe d'utilisateurs peuvent voir le travail.</p>
+<p>Pour les travaux: les utilisateurs n'appartenant pas au groupe d'utilisateurs sélectionné pourront voir les travaux dans la liste de projets,
 mais seront incapables d'y accéder.</p>
 <p>Pour les projets: les utilisateurs n'appartenant pas au groupe d'utilisateurs sélectionné ne pourront pas voir le projet ou le
-tâches associées.</p>
+travaux associées.</p>
 <p>Si aucun groupe d'utilisateurs n'est sélectionné, cette case à cocher n'a aucun effet.</p>
 <a name=\"groupaccess\"><br /></a>
 <p><b>Toute personne du groupe d'utilisateurs peut-elle éditer ?</b></p>
-<p>Cette case à cocher permet à tous les membres d'un groupe d'utilisateurs de modifier la tâche / le projet.  Quand la case est
+<p>Cette case à cocher permet à tous les membres d'un groupe d'utilisateurs de modifier le travail / le projet.  Quand la case est
 cochée, tous les membres du groupe d'utilisateurs peuvent modifier cet élément. Quand la case est décochée, seul le propriétaire
-peut éditer la tâche.</p>
+peut éditer le travail.</p>
 <p>Si aucun groupe d'utilisateurs n'est sélectionné, cette case à cocher n'a aucun effet.</p>
 <br />
 <a name=\"summarypage\"></a>
 <p><b>Sommaire :</b></p>
-<p>La page de résumé contient six colonnes qui indiquent de manière concise ce qui se passe avec chaque tâche.</p>
+<p>La page de résumé contient six colonnes qui indiquent de manière concise ce qui se passe avec chaque travail.</p>
 <ul>
 <li><b>Drapeau</b>:
-indique s'il y a quelque chose de nouveau pour cette tâche.
+indique s'il y a quelque chose de nouveau pour cette travail.
 Les possibilités sont:
 <ul>
 <li><b>C</b>:<br />
-indique que la tâche a été créée<br /></li>
+indique que le travail a été créée<br /></li>
 <li><b>M</b>:<br />
-indique que la tâche a été modifiée<br /></li>
+indique que le travail a été modifiée<br /></li>
 <li><b>P</b>:<br />
-indique qu'une publication a été faite sur le forum de la tâche<br /></li>
+indique qu'une publication a été faite sur le forum du travail<br /></li>
 <li><b>F</b>:<br />
-indique qu'un fichier a été téléchargé pour documenter la tâche<br /></li>
+indique qu'un fichier a été téléchargé pour documenter le travail<br /></li>
 </ul>
-Si un drapeau est présent, vous pouvez cliquer dessus pour afficher la tâche associée.<br /></li>
+Si un drapeau est présent, vous pouvez cliquer dessus pour afficher le travail associé.<br /></li>
 <li><b>Echéance</b>:<br />
-indique quand la tâche est dûe.<br />
-Si la date apparaît en <span class=\"red\">rouge</span>, alors la tâche est en retard; sinon,
+indique quand le travail est dûe.<br />
+Si la date apparaît en <span class=\"red\">rouge</span>, alors le travail est en retard; sinon,
 si la date apparaît en <span class=\"green\">vert</span>,
-alors, la tâche est dûe aujourd'hui.<br /></li>
+alors, le travail est dûe aujourd'hui.<br /></li>
 <li><b>Statut</b>:<br />
 indique le statut de travail de l'élément:
 <ul>
 <li><b>Planifié</b>:<br />
-indique que la tâche a été créée récemment,
-mais (à dessein) n'a pas encore été programmée</li>
+indique que le travail a été créée récemment,
+mais (à dessein) n'a pas encore été programmé</li>
 <li><b>Nouveau</b>:<br />
-indique que la tâche a été créée récemment,
-et attend des ressources pour être démarrée</li>
+indique que le travail a été créée récemment,
+et attend des ressources pour être démarré</li>
 <li><span class=\"blue\">En attente</span>:<br />
-indique que le travail sur la tâche est arrêté,
+indique que le travail sur le travail est arrêté,
 en attente d'un événement externe</li>
 <li><span class=\"orange\">Actif</span>:<br />
-indique que la tâche est en progression</li>
+indique que le travail est en progression</li>
 <li><span class=\"green\">Terminé</span>:<br />
-indique que la tâche est réalisée<br /></li>
+indique que le travail est réalisée<br /></li>
 </ul>
 </li>
 <li><b>Propriétaire</b>:<br />
-indique à qui la tâche est assignée.
+indique à qui le travail est assignée.
 Vous pouvez cliquer sur le nom pour voir plus d'informations au sujet
 de la personne.<br /></li>
 <li><b>Groupe</b>:<br />
-indique le groupe d'utilisateurs ou le groupe de tâches associé à la tâche.
+indique le groupe d'utilisateurs ou le groupe de travaux associé au travail.
 Si vous cliquez sur cet en-tête de colonne,
 l'affichage bascule entre les deux types de groupes.<br /></li>
-<li><b>Tâches</b>:<br />
-indique le nom de la tâche.
-Cliquez sur le nom de la tâche pour accéder à plus d'informations.</li>
+<li><b>travaux</b>:<br />
+indique le nom du travail.
+Cliquez sur le nom du travail pour accéder à plus d'informations.</li>
 </ul>
 ";
   new_box("Aide", $content );
--- webcollab/lang/fr_email.php
+++ webcollab/lang/fr_email.php
@@ -24,7 +24,7 @@
 
   Email text language files for 'fr' (French/Français)
 
-  Translation: Olivier Chaussavoine / Julien Dupont / Martin Brait
+  Translation: Olivier Chaussavoine / Julien Dupont / Martin BRAIT
 
   Maintainer: Julien Dupont
 
@@ -40,101 +40,102 @@ $email_date = sprintf('%s %s %s h %s %+03d00', date('j', $ltime ), $month_array[
 //$email_date = date("d" )." ".$month_array[(date("n" ) )]." ".date('Y g:i a' );
 
 $title_file_post          = ABBR_MANAGER_NAME.": Nouveau Fichier ajouté: %s";
-$email_file_post          = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe qu'un nouveau fichier a été ajouté le ".$email_date." par %1\$s.\n\n".
+$email_file_post          = "Bonjour,\n\n".MANAGER_NAME." vous informe qu'un nouveau fichier a été ajouté le ".$email_date." par %1\$s.\n\n".
                             "Fichier:        %2\$s\n".
                             "Description: %3\$s\n\n".
                             "Projet:         %4\$s\n".
-                            "Tâche:          %5\$s\n\n".
+                            "Travail:          %5\$s\n\n".
                             "Merci de consulter le site pour plus de détails.\n\n".BASE_URL."%6\$s\n";
 
 $title_forum_post         = ABBR_MANAGER_NAME.": Nouveau message: %s";
-$email_forum_post         = "Bonjour,\n\nLe site ".MANAGER_NAME." qui vous informe qu'un nouveau message a été ajouté le ".$email_date." par %1\$s:\n\n".
+$email_forum_post         = "Bonjour,\n\n".MANAGER_NAME." vous informe qu'un nouveau message a été ajouté le ".$email_date." par %1\$s:\n\n".
                             "----\n\n".
                             "%2\$s\n\n".
                             "----\n\n".
                             "Merci de consulter le site pour plus de détails.\n\n".BASE_URL."%3\$s\n";
 
-$email_forum_reply        = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe qu'un nouveau message a été ajouté le ".$email_date." par %1\$s.\n\n".
-                           "Ce message est en réponse à un message précédent de %2\$s.\n\n".
-                           "Message original :\n %3\$s\n\n".
+$email_forum_reply        = "Bonjour,\n\n".MANAGER_NAME." vous informe qu'un nouveau message a été ajouté le ".$email_date." par %1\$s.\n\n".
+                           "Ce message fait suite à un message précédent du %2\$s.\n\n".
+                           "Message original:\n %3\$s\n\n".
                             "----\n\n".
-                           "Nouvelle réponse :\n%4\$s\n\n".
+                           "Nouvelle réponse:\n%4\$s\n\n".
                             "----\n\n".
                             "Merci de consulter le site pour plus de détails.\n\n".BASE_URL."%5\$s\n";
 
-$email_list               = "Projet : %1\$s\n".
-                            "Tâche :  %2\$s\n".
-                            "Statut : %3\$s\n".
-                            "Propriétaire : %4\$s ( %5\$s )\n".
+$email_list               = "Projet: %1\$s\n".
+                            "travail:  %2\$s\n".
+                            "Statut: %3\$s\n".
+                            "Propriétaire: %4\$s ( %5\$s )\n".
                             "Texte:\n%6\$s\n\n".
                             "Merci de consulter le site pour plus de détails.\n\n".BASE_URL."%7\$s\n";
 
 
-$title_takeover_project   = ABBR_MANAGER_NAME.": Votre projet a été affecté."; // You item taken over
-$email_takeover_project   = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe que le projet a été assigné le ".$email_date." par l'administrateur.\n\n";
+$title_takeover_project   = ABBR_MANAGER_NAME.": Votre projet a été affecté"; // You item taken over
+$email_takeover_project   = "Bonjour,\n\n".MANAGER_NAME." vous informe que le projet a été assigné le ".$email_date." par l'administrateur.\n\n";
 
-$title_takeover_task      = ABBR_MANAGER_NAME.": Votre tâche a été affectée."; // You item taken over
-$email_takeover_task      = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe que tâche a été assignée le ".$email_date." par l'administrateur.\n\n";
+$title_takeover_task      = ABBR_MANAGER_NAME.": Votre travail a été affectée"; // You item taken over
+$email_takeover_task      = "Bonjour,\n\n".MANAGER_NAME." vous informe que travail a été assignée le ".$email_date." par l'administrateur.\n\n";
 
 $title_new_owner_project  = ABBR_MANAGER_NAME.": Actualité de vos projets";
-$email_new_owner_project  = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe qu'un projet qui vous appartient a été modifié le ".$email_date.".\n\nPlus de détails :\n\n";
+$email_new_owner_project  = "Bonjour,\n\n".MANAGER_NAME." vous informe qu'un projet vous appartenant a été modifié le ".$email_date.".\n\nPlus de détails:\n\n";
 
-$title_new_owner_task     = ABBR_MANAGER_NAME.": Actualité de vos tâches";
-$email_new_owner_task     = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe que qu'une tâche qui vous appartient a été modifiée le ".$email_date.".\n\nPlus de détails :\n\n";
+$title_new_owner_task     = ABBR_MANAGER_NAME.": Actualité de vos travaux";
+$email_new_owner_task     = "Bonjour,\n\n".MANAGER_NAME." vous informe qu'une travail vous appartenant a été modifiée le ".$email_date.".\n\nPlus de détails:\n\n";
 
-$title_new_group_project  = ABBR_MANAGER_NAME.": Nouveau projet :  %1\$s";
-$email_new_group_project  = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe qu'un nouveau projet a été créé le ".$email_date.".\n\nPlus de détails :\n\n";
+$title_new_group_project  = ABBR_MANAGER_NAME.": Nouveau projet:  %1\$s";
+$email_new_group_project  = "Bonjour,\n\n".MANAGER_NAME." vous informe qu'un nouveau projet a été créé le ".$email_date.".\n\nPlus de détails:\n\n";
 
-$title_new_group_task     = ABBR_MANAGER_NAME.": Nouvelle tâche : %1\$s";
-$email_new_group_task     = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe qu'une nouvelle tâche a été créée le ".$email_date.".\n\nPlus de détails :\n\n";
+$title_new_group_task     = ABBR_MANAGER_NAME.": Nouvelle travail: %1\$s";
+$email_new_group_task     = "Bonjour,\n\n".MANAGER_NAME." vous informe qu'une nouvelle travail a été créée le ".$email_date.".\n\nPlus de détails:\n\n";
 
-$title_edit_owner_project = ABBR_MANAGER_NAME.": Mise à jour de votre projet";
-$email_edit_owner_project ="Bonjour,\n\nLe site ".MANAGER_NAME." vous informe que votre projet a été modifié le ".$email_date.".\n\nPlus de détails :\n\n";
+$title_edit_owner_project = ABBR_MANAGER_NAME.": Mise a jour de votre projet";
+$email_edit_owner_project ="Bonjour,\n\n".MANAGER_NAME." vous informe que votre projet a été modifié le ".$email_date.".\n\nPlus de détails:\n\n";
 
-$title_edit_owner_task    = ABBR_MANAGER_NAME.": Mise a jour de votre tâche";
-$email_edit_owner_task    ="Bonjour,\n\nLe site ".MANAGER_NAME." vous informe que tâche a été modifiée le ".$email_date.".\n\nPlus de détails :\n\n";
+$title_edit_owner_task    = ABBR_MANAGER_NAME.": Mise a jour de votre travail";
+$email_edit_owner_task    ="Bonjour,\n\n".MANAGER_NAME." vous informe que votre travail a été modifiée le ".$email_date.".\n\nPlus de détails:\n\n";
 
 $title_edit_group_project = ABBR_MANAGER_NAME.": Mise à jour d'un projet";
-$email_edit_group_project = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe qu'un projet détenu par %1\$s a été modifié le ".$email_date.".\n\nPlus de détails :\n\n";
+$email_edit_group_project = "Bonjour,\n\n".MANAGER_NAME." vous informe qu'un projet détenu par %1\$s a été modifié le ".$email_date.".\n\nPlus de détails:\n\n";
 
-$title_edit_group_task    = ABBR_MANAGER_NAME.": Mise à jour d'une tâche ";
-$email_edit_group_task    = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe qu'une tâche détenue par %1\$s a été modifiée le ".$email_date.".\n\nPlus de détails :\n\n";
+$title_edit_group_task    = ABBR_MANAGER_NAME.": Mise à jour de tache ";
+$email_edit_group_task    = "Bonjour,\n\n".MANAGER_NAME." vous informe qu'une travail détenue par %1\$s a été modifiée le ".$email_date.".\n\nPlus de détails:\n\n";
 
 $title_delete_project     = ABBR_MANAGER_NAME.": Projet supprimé";
-$email_delete_project     = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe que projet a été effaçé le ".$email_date.".\n\n".
+$email_delete_project     = "Bonjour,\n\n".MANAGER_NAME." vous informe que projet a été effaçé le ".$email_date.".\n\n".
                             "Merci d'avoir géré ce projet jusque-là.\n\n";
 
-$title_delete_task        = ABBR_MANAGER_NAME.": Tâche supprimée";
-$email_delete_task        = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe que votre tâche a été effaçée le ".$email_date.".\n\n".
-                            "Merci d'avoir géré cette tâche jusque-là.\n\n";
+$title_delete_task        = ABBR_MANAGER_NAME.": Tache supprimée";
+$email_delete_task        = "Bonjour,\n\n".MANAGER_NAME." vous informe que travail a été effaçée le ".$email_date.".\n\n".
+                            "Merci d'avoir géré cette tache jusque-là.\n\n";
 
 $delete_list              = "Projet: %1\$s\n".
-                            "Tâche:  %2\$s\n".
+                            "travail:  %2\$s\n".
                             "Statut: %3\$s\n\n".
                             "Texte: \n%4\$s\n\n";
 
-$title_usergroup_add      = ABBR_MANAGER_NAME.": New usergroup %1\$s created";
+$title_usergroup_add      = ABBR_MANAGER_NAME.": Nouveau groupe d'utilisateurs %1\$s créé";
 $email_usergroup_add      = "Bonjour,\n\n".
-                            "Le site ".MANAGER_NAME." vous informe qu'un nouveau groupe d'utilisateurs %1\$s, a été créé le ".$email_date.".\n\n".
-                            "Les membres du nouveau groupe d'utilisateurs sont :\n".
+                            "".MANAGER_NAME." vous informe qu'un nouveau groupe d'utilisateurs %1\$s, a été créé le ".$email_date.".\n\n".
+                            "Les nouveaux membres de ce groupe d'utilisateurs sont :\n".
                             "%2\$s\n";
 
-$title_usergroup_edit      = ABBR_MANAGER_NAME.": Usergroup %1\$s changed";
+$title_usergroup_edit      = ABBR_MANAGER_NAME.": Groupe d'utilisateurs %1\$s modifié";
 $email_usergroup_edit      = "Bonjour,\n\n".
-                            "Le site ".MANAGER_NAME." vous informe que le groupe d'utilisateurs %1\$s, a été modifié le ".$email_date.".\n\n".
-                            "Les membres du groupe d'utilisateurs sont :\n".
+                            "".MANAGER_NAME." vous informe que le groupe d'utilisateurs %1\$s, a été modifié le ".$email_date.".\n\n".
+                            "Les nouveaux membres de ce groupe d'utilisateurs sont :\n".
                             "%2\$s\n";
 
 $title_welcome            = "Bienvenue ".ABBR_MANAGER_NAME;
-$email_welcome            = "Bonjour,\n\nLe site ".MANAGER_NAME." vous accueille le ".$email_date.".\n\n".
-                            "Si vous êtes débutant, voici quelques astuces de prise en main du site :\n\n".
-                            "C'est un outil de gestion de projet. L'écran principal vous montre les projets qui sont actuellement gérés. ".
-                            "Si vous cliquez sur un des noms de projet, vous listez ses tâches. Toutes les informations sont là. ".
-                            "Tous les actualisations que vous effectuez sont transmises aux autres utilisateurs concernés. Réciproquement, vous êtes informés des modifications vous concernant. ".
-                            "Cela vous permet de connaître constamment où en est la réalisation de l'activité.\n\n".
-                            "Vous pouvez aussi devenir propriétaire d'une tâche. Etre propriétaire d'une tâche vous permet d'éditer cette tâche et toutes contributions qui lui sont rattachées. ".
-                            "Renseigner la partie de texte et le statut des tâches permet à tout le monde de connaître leurs progressions. ".
-                            "\n\nBonne découverte, et voici mon email ".EMAIL_ADMIN." en cas de problème.\n\n --Bonne chance !\n\n".
+$email_welcome            = "Bonjour,\n\n. Vous êtes membre du site de gestion de travail collaboratif ".MANAGER_NAME." depuis le ".$email_date.".\n\n".
+                            "Si vous êtes débutant, quelques astuces vous permettront d'utiliser le site rapidement.\n\n".
+                            "".ABBR_MANAGER_NAME." est un outil de gestion de projet. L'écran principal vous montre les projets actuellement gérés. ".
+                            "Si vous cliquez sur un des noms de projet, vous verrez apparaître sa liste des travaux. Toutes les informations sont là. ".
+                            "Tous les actualisations que vous effectuerez concernant un projet, ou une travail, seront transmises aux autres utilisateurs concernés. Réciproquement, vous serez informés des modifications concernant vos travaux ou vos projets. ".
+                            "Cela vous permet de savoir constamment où l'activité se situe.\n\n".
+                            "Vous pouvez initiez un projet, dont vous serez le propriétaire. Pour permettre à d'autre personne de votre groupe de modifier votre projet, vous devez en céder la propriété à votre groupe.\n".
+							"Vous pouvez aussi devenir propriétaire d'une travail, ce qui vous permet d'éditer cette travail et toutes contributions qui y sont rattachées. ".
+                            "Pendant l'utilisation de l'outil, actualisze la partie texte et le statut des travaux pour que tout le monde puisse connaître leurs progressions. ".
+                            "\n\nEn vous souhaitant une bonne découverte, voici mon email : ".EMAIL_ADMIN."s en cas de problème.\n\n --Bonne chance!\n\n".
                             "Profil:       %1\$s\n".
                             "Mot de passe: %2\$s\n\n".
                             "Groupe:       %3\$s".
@@ -143,7 +144,7 @@ $email_welcome            = "Bonjour,\n\nLe site ".MANAGER_NAME." vous accueille
                             "%5\$s";
 
 $title_user_change1       = ABBR_MANAGER_NAME.": Edition de votre profil par l'administrateur";
-$email_user_change1       = "Bonjour,\n\nLe site  ".MANAGER_NAME." vous informe que votre profil a été modifié le ".$email_date." par %1\$s ( %2\$s ).\n\n".
+$email_user_change1       = "Bonjour,\n\n".MANAGER_NAME." vous informe que votre profil a été modifié le ".$email_date." par %1\$s ( %2\$s ).\n\n".
                             "Login:        %3\$s\n".
                             "Mot de passe: %4\$s\n\n".
                             "Groupe:       %5\$s".
@@ -151,20 +152,20 @@ $email_user_change1       = "Bonjour,\n\nLe site  ".MANAGER_NAME." vous informe 
                             "%7\$s";
 
 $title_user_change2       = ABBR_MANAGER_NAME.": Edition de votre profil";
-$email_user_change2       = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe que vous avez correctement modifié votre profil le ".$email_date.".\n\n".
+$email_user_change2       = "Bonjour,\n\n".MANAGER_NAME." vous informe que vous avez correctement modifié votre profil le ".$email_date.".\n\n".
                             "Profil:       %1\$s\n".
                             "Mot de passe: %2\$s\n\n".
                             "Nom:          %3\$s\n";
 
 $title_user_change3       = ABBR_MANAGER_NAME.":  Edition de votre profil";
-$email_user_change3       = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe que vous avez correctement modifié votre profil le ".$email_date.".\n\n".
+$email_user_change3       = "Bonjour,\n\n".MANAGER_NAME." vous informe que vous avez correctement modifié votre profil le ".$email_date.".\n\n".
                             "Profil: %1\$s\n".
                             "Votre mot de passe reste inchangé.\n\n".
                             "Nom:    %2\$s\n";
 
 
 $title_revive             = ABBR_MANAGER_NAME.": Compte réactivé";
-$email_revive             = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe que votre profil a été réactivé le ".$email_date.".\n\n".
+$email_revive             = "Bonjour,\n\n".MANAGER_NAME." vous informe que votre profil a été réactivé le ".$email_date.".\n\n".
                             "Profil: %1\$s\n".
                             "Nom de l'utilisateur: %2\$s\n\n".
                             "Par mesure de sécurité, nous n'envoyons pas votre mot de passe. \n\n".
@@ -173,8 +174,8 @@ $email_revive             = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe q
 
 
 $title_delete_user        = ABBR_MANAGER_NAME.": Profil désactivé.";
-$email_delete_user        = "Bonjour,\n\nLe site ".MANAGER_NAME." vous informe que votre profil a été désactivé le ".$email_date."\n\n".
-                            "Nous somme désolé que vous nous quittiez et vous remercions pour votre participation! \n\n".
+$email_delete_user        = "Bonjour,\n\n".MANAGER_NAME." vous informe que votre profil a été désactivé le ".$email_date."\n\n".
+                            "Nous sommes désolés que vous nous quittiez et vous remercions pour votre participation! \n\n".
                             "Si votre profil a été désactivé par erreur, merci d'envoyer un email à ".EMAIL_ADMIN.".";
 
 ?>
